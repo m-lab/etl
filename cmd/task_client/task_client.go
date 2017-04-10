31a2a9ca2dc99fc9a40f7f1e060509b230e88c3a
Stephen Soltesz <soltesz@google.com>
2017-04-10 18:47:38 -0400

    Add clarifying comments.

--- cmd/task_client/task_client.go
+++ cmd/task_client/task_client.go
@@ -20,10 +20,15 @@ var (
 
 // Create taskqueue.Service for communicating with the taskqueue services.
 func CreateTaskQueueService() *taskqueue.Service {
-
-	// NOTE: DefaultClient uses "Application Default Credentials", which derive
-	// from the GCE VM service account. Appropriate scopes must be set in the
-	// app.yaml configuration, or when the VM is created.
+	// NOTE: google.DefaultClient authenticates the returned client with
+	// "Application Default Credentials". For AppEngine Flex and GCE VMs, the
+	// application default credentials are associated with the default GCE
+	// service account. Credentials are scoped to limit their use to specific
+	// APIs. So, configure necessary scopes in app.yaml, or when creating a GCE
+	// VM; scopes cannot be changed at runtime.
+	//
+	// For more details and background see:
+	//   https://developers.google.com/identity/protocols/application-default-credentials
 	client, err := google.DefaultClient(context.Background(),
 		taskqueue.TaskqueueScope, taskqueue.TaskqueueConsumerScope)
 	if err != nil {
@@ -41,7 +46,7 @@ func CreateTaskQueueService() *taskqueue.Service {
 
 func NewTask(queueName, payload string) *taskqueue.Task {
 	t := &taskqueue.Task{
-		// Set first available least time to now.
+		// Set first available lease time to now.
 		LeaseTimestamp: time.Now().Unix(),
 		PayloadBase64:  payload,
 		QueueName:      queueName,
@@ -50,12 +55,18 @@ func NewTask(queueName, payload string) *taskqueue.Task {
 }
 
 func main() {
+	flag.Parse()
+
 	api := CreateTaskQueueService()
 	pretty.Print(api)
 
 	req := NewTask(*queue, *payload)
 	pretty.Print(req)
 
+	// WARNING: NOTE: The v1beta2 taskqueue REST API requires the prefix "p~"
+	// before the project name. Without this prefix, Insert, Lease, and Delete
+	// operations fail with 403 errors:
+	//    "you are not allowed to make this api call".
 	task, err := api.Tasks.Insert(fmt.Sprintf("p~%s", *project), *queue, req).Do()
 	if err != nil {
 		panic(err)
