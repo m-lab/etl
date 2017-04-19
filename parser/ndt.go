b49f307fb72b416d7c17ca3e75af952ebbaed228
Stephen Soltesz <soltesz@google.com>
2017-04-19 13:45:06 -0400

    Add a basic NDT parser implementation.

---
+++ parser/ndt.go
@@ -0,0 +1,65 @@
+package parser
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"github.com/m-lab/etl/web100"
+)
+
+type NDTParser struct {
+	Parser
+	tmpDir string
+}
+
+func (n *NDTParser) Parse(fn string, table string, rawSnapLog []byte) (interface{}, error) {
+	// Write rawSnapLog to /mnt/tmpfs.
+	tmpFile := fmt.Sprintf("%s/%s", n.tmpDir, fn)
+	err := ioutil.WriteFile(tmpFile, rawSnapLog, 0644)
+	if err != nil {
+		return nil, err
+	}
+	// TODO(dev): log possible remove errors.
+	defer os.Remove(tmpFile)
+
+	// Parse the tcp-kis.txt web100 variable definition file.
+	data, err := web100.Asset("tcp-kis.txt")
+	if err != nil {
+		// Asset missing from build.
+		return nil, err
+	}
+	b := bytes.NewBuffer(data)
+	legacyNames, err := web100.ParseWeb100Definitions(b)
+	if err != nil {
+		return nil, err
+	}
+
+	// Open the file we created above.
+	w, err := web100.Open(tmpFile, legacyNames)
+	if err != nil {
+		return nil, err
+	}
+	defer w.Close()
+
+	// Find the last web100 snapshot.
+	for {
+		err = w.Next()
+		if err != nil {
+			break
+		}
+	}
+	// We expect EOF.
+	if err != io.EOF {
+		return nil, err
+	}
+
+	// Extract the values from the last snapshot.
+	results, err := w.Values()
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
---
+++ parser/ndt_test.go
@@ -0,0 +1,59 @@
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+)
+
+func TestNDTParser(t *testing.T) {
+	// Load test data.
+	rawData, err := ioutil.ReadFile("testdata/c2s_snaplog")
+
+	n := NDTParser{tmpDir: "./"}
+	values, err := n.Parse("filename", "table", rawData)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	results := values.(map[string]bigquery.Value)
+	// TODO(dev): find a better way to verify the returned values are correct.
+	expectedValues := map[string]bigquery.Value{
+		"web100_log_entry.version":                    "2.5.27 201001301335 net100",
+		"web100_log_entry.snap.RemAddress":            "45.56.98.222",
+		"web100_log_entry.connection_spec.local_port": int64(43685),
+	}
+	for key, value := range expectedValues {
+		// Raw bigquery.Value instances do not compare.
+		if results[key] != value {
+			t.Errorf("Wrong value for %q: got %q; want %q", key, str(results[key]), str(value))
+		}
+	}
+	// TODO(dev): remove print of entire snaplog.
+	prettyPrint(r)
+}
+
+// TODO(dev): is there a better way to display these values?
+func str(v bigquery.Value) string {
+	switch v.(type) {
+	case string:
+		s := v.(string)
+		return s
+	case int64:
+		i := v.(int64)
+		return fmt.Sprintf("%d", i)
+	default:
+		panic("Only string and int64 types are supported.")
+	}
+}
+
+func prettyPrint(results map[string]bigquery.Value) {
+	b, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(b))
+}
