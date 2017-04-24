423a04344da6d4dc264edeb13f7370a47420f239
Peter Boothe <pboothe@pboothe2.nyc.corp.google.com>
2017-04-24 18:14:59 -0400

    Multiple queues

--- appengine/queue_pusher/queue_pusher.go
+++ appengine/queue_pusher/queue_pusher.go
@@ -16,19 +16,6 @@ import (
 
 const defaultMessage = "<html><body>This is not the app you're looking for.</body></html>"
 
-type experimentDataType uint8
-const (
-	NDT experimentDataType = iota + 1
-	SIDESTREAM
-	TRACEROUTE
-	DISCO
-)
-
-struct TaskInfo {
-	re *regexp.Regexp
-        kind experimentDataType
-}
-
 // Requests can only add tasks to one of these whitelisted queue names.
 var queueWhitelist = map[string]*regexp.Regexp{
 	"etl-ndt-queue":        regexp.MustCompile("/ndt/\\d{4}/\\d{2}/\\d{2}/[^/]*.tgz"),
@@ -111,25 +98,18 @@ func receiver(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// determine correct queue based on file name.
-	queuename := ""
-	for queue, re := range queueWhitelist {
-		if re.Find(decoded_filename) != nil {
-			queuename = queue
-			break
-		}
-	}
+	queuename := queueForFile(decoded_filename)
+
 	// Lots of files will be archived that should not be enqueued. Pass
 	// over those files without comment.
-	if queuename == "" {
-		return
-	}
-
-	ctx := appengine.NewContext(r)
-	params := url.Values{"filename": []string{decoded_filename}}
-	t := taskqueue.NewPOSTTask("/worker", params)
-	if _, err := taskqueue.Add(ctx, t, queuename); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+	if queuename != "" {
+		ctx := appengine.NewContext(r)
+		params := url.Values{"filename": []string{decoded_filename}}
+		t := taskqueue.NewPOSTTask("/worker", params)
+		if _, err := taskqueue.Add(ctx, t, queuename); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 
 	// determine correct queue based on file name.
