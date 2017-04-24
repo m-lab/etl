44baa1494d2ede77f0935eee4651bee5676775ea
Greg Russell <gfr10598@gmail.com>
2017-04-24 16:02:41 -0400

    hack multiple test type handling

---
+++ etl/globals.go
@@ -0,0 +1,25 @@
+package etl
+
+import (
+	"regexp"
+)
+
+const date = `(\d{4}/[01]\d/[0123]\d)`
+
+// These are here to facilitate use across queue-pusher and parsing components.
+var (
+	// This matches any valid test file name, and some invalid ones.
+	// Field 2 contains the test type.
+	//                                  1     2         3         4
+	TaskPattern = regexp.MustCompile(`(.*)/([^/]*)/` + date + `/([^/]*).tgz`)
+)
+
+type DataType int
+
+const (
+	TypeInvalid DataType = iota
+	TypeNDT
+	TypeSS
+	TypePT
+	TypeSW
+)
