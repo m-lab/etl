0df27c38e6c2b1a1749b8901bac26342619510a2
Greg Russell <gfr10598@gmail.com>
2017-04-27 16:25:05 -0400

    Add disco parser and unit tests

---
+++ parser/disco.go
@@ -0,0 +1,97 @@
+// The parser package defines the Parser interface and implementations for the different
+// test types, NDT, Paris Traceroute, and SideStream.
+package parser
+
+// This file defines the Parser subtype that handles DISCO data.
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/prometheus/client_golang/prometheus"
+
+	"github.com/m-lab/etl/etl"
+)
+
+//=====================================================================================
+//                       Disco Parser
+//=====================================================================================
+type PortStats struct {
+	// TODO - replace these with standard meta data.
+	Meta struct {
+		FileName  string `json:"filename, string"`
+		TestName  string `json:"testname, string"`
+		ParseTime int64  `json:"parsetime, int64"`
+	} `json:"meta"`
+
+	Sample []struct { //    []Sample `json: "sample"`
+		Timestamp int64   `json:"timestamp, int64"`
+		Value     float32 `json:"value, float32"`
+	} `json:"sample"`
+	Metric     string `json:"metric"`
+	Hostname   string `json:"hostname"`
+	Experiment string `json:"experiment"`
+
+	// bigquery doesn't handle maps within structs.  8-(
+	// Meta       map[string]bigquery.Value `json:"meta"`
+}
+
+// TODO(dev) add tests
+type DiscoParser struct {
+	inserter etl.Inserter
+}
+
+func NewDiscoParser(ins etl.Inserter) etl.Parser {
+	return &DiscoParser{inserter: ins}
+}
+
+// Disco data a JSON representation that should be pushed directly into BigQuery.
+// For now, though, we translate it into a map, for compatibility with current inserter
+// backend.
+//
+// Returns:
+//   error on Decode error
+//   error on InsertRows error
+//   nil on success
+//
+// TODO - optimize this to use the JSON directly, if possible.
+func (dp *DiscoParser) ParseAndInsert(meta map[string]bigquery.Value, testName string, test []byte) error {
+	testCount.With(prometheus.Labels{"table": dp.TableName()}).Inc()
+
+	meta["testname"] = testName
+	ms := struct {
+		FileName  string `json:"filename, string"`
+		TestName  string `json:"testname, string"`
+		ParseTime int64  `json:"parsetime, int64"`
+	}{meta["filename"].(string), meta["testname"].(string), meta["parse_time"].(time.Time).Unix()}
+
+	rdr := bytes.NewReader(test)
+	dec := json.NewDecoder(rdr)
+	for dec.More() {
+		var ps PortStats
+		ps.Meta = ms
+		err := dec.Decode(&ps)
+		if err != nil {
+			// TODO(dev) Should accumulate errors, instead of aborting?
+			return err
+		}
+		err = dp.inserter.InsertRow(ps)
+		if err != nil {
+			switch t := err.(type) {
+			case bigquery.PutMultiError:
+				log.Printf("%v\n", t[0].Error())
+			default:
+			}
+			// TODO(dev) Should accumulate errors, instead of aborting?
+			return err
+		}
+	}
+	return nil
+}
+
+func (dp *DiscoParser) TableName() string {
+	return dp.inserter.TableName()
+}
---
+++ parser/disco_test.go
@@ -0,0 +1,115 @@
+package parser_test
+
+import (
+	"fmt"
+	"log"
+	"testing"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+
+	"github.com/m-lab/etl/bq"
+	"github.com/m-lab/etl/etl"
+	"github.com/m-lab/etl/fake"
+	"github.com/m-lab/etl/parser"
+)
+
+func init() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+}
+
+// PrintingInserter prints out the items passed in.
+// Inject into Parser for testing.
+type PrintingInserter struct {
+	bq.NullInserter
+}
+
+func (ti *PrintingInserter) InsertRow(data interface{}) error {
+	fmt.Printf("%T: %v\n", data, data)
+	return nil
+}
+func (ti *PrintingInserter) Flush() error {
+	return nil
+}
+
+var test_data []byte = []byte(`{
+	"sample": [{"timestamp": 69850, "value": 0.0}, {"timestamp": 69860, "value": 0.0}],
+	"metric": "switch.multicast.local.rx",
+	"hostname": "mlab4.sea05.measurement-lab.org",
+	"experiment": "s1.sea05.measurement-lab.org"}
+	{"sample": [{"timestamp": 69870, "value": 0.0}, {"timestamp": 69880, "value": 0.0}],
+	"metric": "switch.multicast.local.rx",
+	"hostname": "mlab1.sea05.measurement-lab.org",
+	"experiment": "s1.sea05.measurement-lab.org"}`)
+
+// This tests the parser, using a fake inserter, so that it runs entirely locally.
+func TestJSONParsing(t *testing.T) {
+	// This creates a real inserter, with a fake uploader, for local testing.
+	uploader := fake.FakeUploader{}
+	ins, err := bq.NewInserter(etl.InserterParams{
+		"mlab_sandbox", "disco_test", 10 * time.Second, 3}, &uploader)
+
+	var parser etl.Parser = parser.NewDiscoParser(ins)
+
+	meta := map[string]bigquery.Value{"filename": "filename", "parse_time": time.Now()}
+	// Should result in two tests sent to inserter, but no call to uploader.
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+	if ins.Count() != 2 {
+		t.Error("Count = ", ins.Count())
+		t.Fail()
+	}
+
+	// Adds two more rows, triggering an upload of 3 rows.
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+	if len(uploader.Rows) != 3 {
+		t.Error("Uploader Row Count = ", len(uploader.Rows))
+	}
+
+	// Adds two more rows, triggering an upload of 3 rows.
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+
+	if ins.Count() != 6 {
+		t.Error("Count = ", ins.Count())
+	}
+	if ins.RowsInBuffer() != 0 {
+		t.Error("RowsInBuffer = ", ins.RowsInBuffer())
+	}
+	if len(uploader.Rows) != 3 {
+		t.Error("Uploader Row Count = ", len(uploader.Rows))
+	}
+
+	log.Printf("%v\n", uploader.Request)
+	// TODO - check something
+
+	if err != nil {
+		log.Printf("%v\n", uploader.Request)
+		log.Printf("%d Rows\n", len(uploader.Rows))
+		log.Printf("%v\n", uploader.Rows[0])
+		t.Error(err)
+	}
+}
+
+// DISABLED
+// This tests insertion into a test table in the cloud.  Should not normally be executed.
+func xTestRealBackend(t *testing.T) {
+	ins, err := bq.NewInserter(etl.InserterParams{
+		"mlab_sandbox", "disco_test", 10 * time.Second, 3}, nil)
+
+	var parser etl.Parser = parser.NewDiscoParser(ins)
+
+	meta := map[string]bigquery.Value{"filename": "filename", "parse_time": time.Now()}
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+	err = parser.ParseAndInsert(meta, "testName", test_data)
+
+	if ins.Count() != 6 {
+		t.Error("Count = ", ins.Count())
+	}
+	if ins.RowsInBuffer() != 0 {
+		t.Error("RowsInBuffer = ", ins.RowsInBuffer())
+	}
+
+	if err != nil {
+		t.Error(err)
+	}
+}
