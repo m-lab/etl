e20edfd9c48471fa6a3b236bb46c8805b4a98fd0
yachang <yachang@users.noreply.github.com>
2017-05-03 13:47:50 -0400

    Create pt_test.go

---
+++ parser/pt_test.go
@@ -0,0 +1,27 @@
+package parser
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+// TODO: IPv6 tests
+func TestGetIPTuple(t *testing.T) {
+	fn1 := PTFileName{name: "20170320T23:53:10Z-98.162.212.214-53849-64.86.132.75-42677.paris"}
+	dest_ip, dest_port, server_ip, server_port := fn1.GetIPTuple()
+	if dest_ip != "98.162.212.214" || dest_port != "53849" || server_ip != "64.86.132.75" || server_port != "42677" {
+		t.Errorf("Wrong!\n")
+		return
+	}
+
+}
+
+func TestPTParser(t *testing.T) {
+	rawData, err := ioutil.ReadFile("testdata/20170320T23:53:10Z-98.162.212.214-53849-64.86.132.75-42677.paris")
+
+	n := PTParser{tmpDir: "./"}
+	err = n.ParseAndInsert(nil, "testdata/20170320T23:53:10Z-98.162.212.214-53849-64.86.132.75-42677.paris", rawData)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
