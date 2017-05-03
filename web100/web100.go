c9155056655e353ccc605296be745803279f8389
Stephen Soltesz <soltesz@google.com>
2017-05-03 11:39:23 -0400

    Add NDT support for full schema.

--- web100/web100.go
+++ web100/web100.go
@@ -21,11 +21,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"strconv"
 	"sync"
 	"unsafe"
 
 	"cloud.google.com/go/bigquery"
+	"github.com/m-lab/etl/schema"
 )
 
 var (
@@ -45,6 +47,9 @@ type Web100 struct {
 	// Do not export unsafe pointers.
 	snaplog unsafe.Pointer
 	snap    unsafe.Pointer
+	// temp space for converting web100 variables to string.
+	text unsafe.Pointer
+	data unsafe.Pointer
 
 	// The original filename created by the NDT server.
 	TestId string
@@ -93,6 +98,9 @@ func Open(filename string, legacyNames map[string]string) (*Web100, error) {
 		legacyNames: legacyNames,
 		snaplog:     unsafe.Pointer(snaplog),
 		snap:        unsafe.Pointer(snap),
+		// Pre-allocate space for converting snapshot values.
+		text: C.calloc(2*C.WEB100_VALUE_LEN_MAX, 1),
+		data: C.calloc(C.WEB100_VALUE_LEN_MAX, 1),
 	}
 	return w, nil
 }
@@ -119,98 +127,81 @@ func (w *Web100) Next() error {
 }
 
 func (w *Web100) Values() (map[string]bigquery.Value, error) {
-	v, err := w.logValues()
+	results := schema.NewRecord()
+	v, err := w.logValues(schema.Map(results["web100_log_entry"]))
 	if err != nil {
 		return nil, err
 	}
-	v, err = w.snapValues(v)
+	err = w.snapValues(v)
 	if err != nil {
 		return nil, err
 	}
-	return v, nil
+	// TODO(dev): is NPAD also affected in the same way?
+	err = fixValues(results["web100_log_entry"])
+	if err != nil {
+		return nil, err
+	}
+	return results, nil
+}
+
+// fixValues updates web100 log values that need post-processing fix-ups.
+// TODO(dev): does this only apply to NDT or is NPAD also affected?
+func fixValues(r bigquery.Value) error {
+	record := schema.Map(r)
+	if record == nil {
+		return fmt.Errorf("Can only fix types of map[string]bigquery.Value")
+	}
+	// TODO(dev): fix these values.
+	// Fix StartTimeStamp:
+	//  - web100_log_entry.snap.StartTimeStamp: (1000000 * StartTimeStamp + StartTimeUsec)
+	// Fix IPv6 addresses in connection_spec:
+	//  - web100_log_entry.connection_spec.local_ip
+	//  - web100_log_entry.connection_spec.remote_ip
+	// Fix local_af:
+	//  - web100_log_entry.connection_spec.local_af: IPv4 = 0, IPv6 = 1.
+	return nil
 }
 
 // logValues returns a map of values from the web100 log. IPv6 address
 // connection information is not available and must be set based on a snapshot.
-func (w *Web100) logValues() (map[string]bigquery.Value, error) {
+func (w *Web100) logValues(web100LogEntry map[string]bigquery.Value) (map[string]bigquery.Value, error) {
 	snaplog := (*C.web100_log)(w.snaplog)
 	agent := C.web100_get_log_agent(snaplog)
 
-	results := map[string]bigquery.Value{
-		"test_id":  w.TestId,
-		"log_time": w.LogTime,
-		"connection_spec": map[string]bigquery.Value{
-			"server_ip":             "",
-			"server_af":             0,
-			"server_hostname":       "",
-			"server_kernel_version": "",
-			"client_ip":             "",
-			"client_af":             0,
-			"client_hostname":       "",
-			"client_os":             "",
-			"client_kernel_version": "",
-			"client_version":        "",
-			"client_browser":        "",
-			"client_application":    "",
-			"data_direction":        0,
-			// TODO(prod): add geolocation sub-records.
-			//  client_geolocation: record
-			//  server_geolocation: record
-		},
-		"web100_log_entry": map[string]bigquery.Value{
-			"version":  C.GoString(C.web100_get_agent_version(agent)),
-			"log_time": int64(C.web100_get_log_time(snaplog)),
-			"connection_spec": map[string]bigquery.Value{
-				"remote_ip":   "",
-				"remote_port": 0,
-				"local_ip":    "",
-				"local_af":    0,
-				"local_port":  0,
-			},
-			"snap": make(map[string]bigquery.Value),
-		},
-	}
+	web100LogEntry["version"] = C.GoString(C.web100_get_agent_version(agent))
+	web100LogEntry["log_time"] = int64(C.web100_get_log_time(snaplog))
 
-	web100LogEntry := results["web100_log_entry"].(map[string]bigquery.Value)
-	connectionSpec := web100LogEntry["connection_spec"].(map[string]bigquery.Value)
 	conn := C.web100_get_log_connection(snaplog)
-	// NOTE: web100_connection_spec_v6 is not filled in by the web100 library.
-	// NOTE: addrtype is always WEB100_ADDRTYPE_UNKNOWN.
-	// NOTE: legacy values for local_af are: IPv4 = 0, IPv6 = 1.
-	connectionSpec["local_af"] = int64(0)
 
+	// NOTE: web100_connection_spec_v6 is never set by the web100 library.
+	// NOTE: conn->addrtype is always WEB100_ADDRTYPE_UNKNOWN.
+	// So, we expect it to be IPv4 and fix local_ip and remote_ip later if
+	// snapshots have IPv6 addresses.
 	var spec C.struct_web100_connection_spec
 	C.web100_get_connection_spec(conn, &spec)
 
-	// TODO(prod): do not use inet_ntoa because it depends on a static internal buffer.
-	/* inet_ntoa(their_addr.sin_addr), */ // IPv4
-	// inet_ntop(AF_INET6, &their_addr.sin6_addr, buf, sizeof(buf)), // IPv6
-
-	addr := C.struct_in_addr{C.in_addr_t(spec.src_addr)}
-	connectionSpec["local_ip"] = C.GoString(C.inet_ntoa(addr))
+	// NOTE: web100_connection_spec only contains IPv4 addresses (4 byte values).
+	// If the connection was IPv6, the IPv4 addresses here will be 0.0.0.0.
+	srcIp := net.IP(C.GoBytes(unsafe.Pointer(&spec.src_addr), 4))
+	dstIp := net.IP(C.GoBytes(unsafe.Pointer(&spec.dst_addr), 4))
+	connectionSpec := schema.Map(web100LogEntry["connection_spec"])
+	connectionSpec["local_ip"] = srcIp.String()
 	connectionSpec["local_port"] = int64(spec.src_port)
-
-	addr = C.struct_in_addr{C.in_addr_t(spec.dst_addr)}
-	connectionSpec["remote_ip"] = C.GoString(C.inet_ntoa(addr))
+	connectionSpec["remote_ip"] = dstIp.String()
 	connectionSpec["remote_port"] = int64(spec.dst_port)
 
-	return results, nil
+	// NOTE: legacy values of local_af are: IPv4 = 0, IPv6 = 1.
+	connectionSpec["local_af"] = int64(0)
+
+	return web100LogEntry, nil
 }
 
 // snapValues converts all variables in the latest snap record into a results map.
-func (w *Web100) snapValues(logValues map[string]bigquery.Value) (map[string]bigquery.Value, error) {
+func (w *Web100) snapValues(logValues map[string]bigquery.Value) error {
 	snaplog := (*C.web100_log)(w.snaplog)
 	snap := (*C.web100_snapshot)(w.snap)
 
-	// TODO(dev): do not re-allocate these buffers on every call.
-	var_text := C.calloc(2*C.WEB100_VALUE_LEN_MAX, 1) // Use a better size.
-	defer C.free(var_text)
-
-	var_data := C.calloc(C.WEB100_VALUE_LEN_MAX, 1)
-	defer C.free(var_data)
-
-	web100log := logValues["web100_log_entry"].(map[string]bigquery.Value)
-	web100snap := web100log["snap"].(map[string]bigquery.Value)
+	web100snap := schema.Map(logValues["snap"])
 
 	// Parses variables from most recent web100_snapshot data.
 	var w_errno C.int = C.WEB100_ERR_SUCCESS
@@ -224,34 +215,33 @@ func (w *Web100) snapValues(logValues map[string]bigquery.Value) (map[string]big
 		var_type := C.web100_get_var_type(v)
 
 		// Read the raw variable data from the snapshot data.
-		errno := C.web100_snap_read(v, snap, var_data)
+		errno := C.web100_snap_read(v, snap, w.data)
 		if errno != C.WEB100_ERR_SUCCESS {
-			return nil, fmt.Errorf(C.GoString(C.web100_strerror(errno)))
+			return fmt.Errorf(C.GoString(C.web100_strerror(errno)))
 		}
 
-		// Convert raw var_data into a string based on var_type.
+		// Convert raw w.data into a string based on var_type.
 		// TODO(prod): reimplement web100_value_to_textn to operate on Go types.
-		C.web100_value_to_textn((*C.char)(var_text), C.WEB100_VALUE_LEN_MAX, (C.WEB100_TYPE)(var_type), var_data)
+		C.web100_value_to_textn((*C.char)(w.text), C.WEB100_VALUE_LEN_MAX,
+			(C.WEB100_TYPE)(var_type), w.data)
 
 		// Use the canonical variable name.
-		var canonicalName string
+		canonicalName := C.GoString(name)
 		if _, ok := w.legacyNames[canonicalName]; ok {
 			canonicalName = w.legacyNames[canonicalName]
-		} else {
-			canonicalName = C.GoString(name)
 		}
 
 		// TODO(dev): are there any cases where we need unsigned int64?
 		// Attempt to convert the current variable to an int64.
-		value, err := strconv.ParseInt(C.GoString((*C.char)(var_text)), 10, 64)
+		value, err := strconv.ParseInt(C.GoString((*C.char)(w.text)), 10, 64)
 		if err != nil {
 			// Leave variable as a string.
-			web100snap[canonicalName] = C.GoString((*C.char)(var_text))
+			web100snap[canonicalName] = C.GoString((*C.char)(w.text))
 		} else {
 			web100snap[canonicalName] = value
 		}
 	}
-	return logValues, nil
+	return nil
 }
 
 // Close releases resources created by Open.
@@ -263,6 +253,14 @@ func (w *Web100) Close() error {
 	err := C.web100_log_close_read(snaplog)
 	if err != C.WEB100_ERR_SUCCESS {
 		return fmt.Errorf(C.GoString(C.web100_strerror(err)))
+	}
+	if w.text != nil {
+		C.free(w.text)
+		w.text = nil
+	}
+	if w.data != nil {
+		C.free(w.data)
+		w.data = nil
 	}
 
 	// Clear pointer after free.
