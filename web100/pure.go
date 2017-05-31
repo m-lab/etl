8e516f6c900aa6d665aeea2b9ab55b3e34d5b59f
Greg Russell <gfr10598@gmail.com>
2017-05-31 13:49:15 -0400

    Some subtleties

--- web100/pure.go
+++ web100/pure.go
@@ -2,10 +2,13 @@ package web100
 
 import (
 	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
 	"io"
 )
+
+// TODO - resolve use of Record, Snapshot, slog, snap, etc.
 
 // Need to:
 // 1. Read the header
@@ -41,8 +44,10 @@ import (
 //00000cc0  61 74 61 2d 2d 2d 2d 0a  00 00 00 00 71 46 71 46  |ata----.....qFqF|
 
 const (
-	BEGIN_SNAP_DATA = "----Begin-Snap-Data----"           // Plus a newline?
-	END_OF_HEADER   = "\x00----End-Of-Header---- -1 -1\n" // No newline.
+	BEGIN_SNAP_DATA   = "----Begin-Snap-Data----"           // Plus a newline?
+	END_OF_HEADER     = "\x00----End-Of-Header---- -1 -1\n" // No newline.
+	GROUPNAME_LEN_MAX = 32
+	VARNAME_LEN_MAX   = 32
 )
 
 type VarType int
@@ -110,7 +115,7 @@ func NewVariable(s *string) (*Variable, error) {
 type FieldSet struct {
 	Fields       []Variable
 	FieldMap     map[string]int // Map from field name to index in Fields.
-	RecordLength int            // Total length of record, including BEGIN_SNAP_DATA
+	RecordLength int            // Total length of record, including preamble, e.g. BEGIN_SNAP_DATA
 }
 
 // Find returns the variable of a given name, or nil.
@@ -122,10 +127,20 @@ func (fs *FieldSet) Find(name string) *Variable {
 	return &fs.Fields[index]
 }
 
+type ConnectionSpec struct {
+	DestPort uint16
+	SrcPort  uint16
+	DestAddr []byte
+	SrcAddr  []byte
+}
+
 type SnapLog struct {
 	// The entire raw contents of the file.  Generally 1.5 MB, but may be
 	// much larger
 	raw []byte
+
+	LogTime   uint32
+	GroupName string
 
 	ConnSpecOffset int // Offset in bytes of the ConnSpec
 	BodyOffset     int // Offset in bytes of the first snapshot
@@ -133,7 +148,7 @@ type SnapLog struct {
 	Body           FieldSet
 	Tune           FieldSet
 
-	ConnSpec map[string]interface{}
+	ConnSpec ConnectionSpec
 }
 
 func parseFields(buf *bytes.Buffer, preamble string, terminator string) (FieldSet, error) {
@@ -173,6 +188,33 @@ func parseFields(buf *bytes.Buffer, preamble string, terminator string) (FieldSe
 	}
 }
 
+func parseConnectionSpec(buf *bytes.Buffer) (ConnectionSpec, error) {
+	// The web100 snaplog only correctly represents ipv4 addresses.
+	// But try to read it anyway.
+	raw := make([]byte, 16)
+	n, err := buf.Read(raw)
+	if err != nil || n < 16 {
+		return ConnectionSpec{}, errors.New("Too few bytes for connection spec")
+	}
+	dstPort := binary.LittleEndian.Uint16(raw[0:2])
+	// WARNING - the web100 code seemingly depends on a 32 bit architecture.
+	// There is no "packed" directive for the web100_connection_spec, and the
+	// fields all seem to be 32 bit aligned.
+	dstAddr := raw[4:8]
+	srcPort := binary.LittleEndian.Uint16(raw[8:10])
+	srcAddr := raw[12:16]
+
+	return ConnectionSpec{DestPort: dstPort, SrcPort: srcPort,
+		DestAddr: dstAddr, SrcAddr: srcAddr}, nil
+}
+
+/*struct web100_connection_spec {
+    u_int16_t dst_port;
+    u_int32_t dst_addr;
+    u_int16_t src_port;
+    u_int32_t src_addr;
+};*/
+
 // Wraps a byte array in a SnapLog.  Returns error if there are problems.
 func NewSnapLog(raw []byte) (*SnapLog, error) {
 	buf := bytes.NewBuffer(raw)
@@ -196,27 +238,70 @@ func NewSnapLog(raw []byte) (*SnapLog, error) {
 	if err != nil {
 		return nil, err
 	}
+	spec.RecordLength += 0
 
 	body, err := parseFields(buf, "/read\n", "\n")
 	if err != nil {
 		return nil, err
 	}
+	// There seems to be a null character at the end of each record, so
+	// add one to the length.
+	body.RecordLength += len(BEGIN_SNAP_DATA) + 1
 
 	tune, err := parseFields(buf, "/tune\n", END_OF_HEADER)
 	if err != nil {
 		return nil, err
 	}
 
-	// Now, parse the connection spec.
+	// Read the timestamp and groupname
+	t := make([]byte, 4)
+	n, err := buf.Read(t)
+	if err != nil || n < 4 {
+		return nil, errors.New("Too few bytes for logTime")
+	}
+	logTime := binary.LittleEndian.Uint32(t)
+	fmt.Println(logTime)
+
+	gn := make([]byte, GROUPNAME_LEN_MAX)
+	n, err = buf.Read(gn)
+	if err != nil || n != GROUPNAME_LEN_MAX {
+		return nil, errors.New("Too few bytes for groupName")
+	}
+	groupName := string(gn)
+	fmt.Println(groupName)
+
 	connSpecOffset := len(raw) - buf.Len()
-	// ...
-	//
+	connSpec, err := parseConnectionSpec(buf)
+	if err != nil {
+		return nil, err
+	}
+
 	bodyOffset := len(raw) - buf.Len()
 
-	slog := SnapLog{raw: raw, ConnSpecOffset: connSpecOffset, BodyOffset: bodyOffset,
-		Spec: spec, Body: body, Tune: tune, ConnSpec: nil}
+	slog := SnapLog{raw: raw, LogTime: logTime, GroupName: groupName,
+		ConnSpecOffset: connSpecOffset, BodyOffset: bodyOffset,
+		Spec: spec, Body: body, Tune: tune, ConnSpec: connSpec}
 
 	return &slog, nil
+}
+
+func (sl *SnapLog) Validate() error {
+	// Verify that body starts with BEGIN
+	first := string(sl.raw[sl.BodyOffset : sl.BodyOffset+len(BEGIN_SNAP_DATA)])
+	if first != BEGIN_SNAP_DATA {
+		return errors.New("Missing first BeginSnapData")
+	}
+
+	// Verify that body size is integer multiple of body record length.
+	total := len(sl.raw) - sl.BodyOffset
+	if total%sl.Body.RecordLength != 0 {
+		return errors.New("Body is not multiple of Body.RecordLength")
+	}
+
+	// Verify that last record is good quality
+
+	// Verify that last record is in a TCP end state?
+	return nil
 }
 
 type Snapshot struct {
@@ -225,5 +310,6 @@ type Snapshot struct {
 
 // Returns the snapshot at index n, or error if n is not a valid index, or data is corrupted.
 func (slog *SnapLog) Snapshot(n int) (Snapshot, error) {
+	// TODO
 	return Snapshot{}, nil
 }
--- web100/pure_test.go
+++ web100/pure_test.go
@@ -11,26 +11,49 @@ import (
 func TestValidation(t *testing.T) {
 }
 
-func TestWeb100(t *testing.T) {
+func TestHeaderParsing(t *testing.T) {
 	c2sName := `20170509T13:45:13.590210000Z_eb.measurementlab.net:48716.c2s_snaplog`
 	c2sData, err := ioutil.ReadFile(`testdata/` + c2sName)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 
-	log, err := web100.NewSnapLog(c2sData)
+	slog, err := web100.NewSnapLog(c2sData)
 
 	if err != nil {
 		t.Fatal(err.Error())
 	}
-	if len(log.Body.Fields) != 142 {
-		fmt.Printf("%d %v\n", len(log.Body.Fields), log.Body)
+	if len(slog.Body.Fields) != 142 {
+		fmt.Printf("%d %v\n", len(slog.Body.Fields), slog.Body)
 		t.Error("Wrong number of fields.")
 	}
-	if log.Body.RecordLength != 645 {
-		fmt.Printf("Record length %d\n", log.Body.RecordLength)
+	if slog.Body.RecordLength != 669 {
+		fmt.Printf("Record length %d\n", slog.Body.RecordLength)
 		t.Error("Wrong record length.")
 	}
-	fmt.Println(log.BodyOffset)
-	fmt.Println(log.ConnSpecOffset)
+	fmt.Printf("%d %x\n", slog.Spec.RecordLength, slog.ConnSpecOffset)
+	fmt.Printf("%d %x\n", slog.Body.RecordLength, slog.BodyOffset)
+	fmt.Printf("%x\n", slog.Body.RecordLength+slog.BodyOffset)
+
+	if slog.LogTime != 1494337516 {
+		t.Error("Incorrect LogTime.")
+	}
+	if err = slog.Validate(); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSnapshotContent(t *testing.T) {
+	c2sName := `20170509T13:45:13.590210000Z_eb.measurementlab.net:48716.c2s_snaplog`
+	c2sData, err := ioutil.ReadFile(`testdata/` + c2sName)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	slog, err := web100.NewSnapLog(c2sData)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	_, err = slog.Snapshot(1)
+
 }
