e52702deb1e708c89ae9697a74adfb48536054fc
Greg Russell <gfr10598@gmail.com>
2017-06-23 10:46:00 -0400

    this actually works on sandbox

--- functions/embargo/transfer.js
+++ functions/embargo/transfer.js
@@ -2,14 +2,22 @@
  * @fileoverview Description of this file.
   To deploy this cloud function to mlab-sandbox, try:
 
-  gcloud beta functions deploy fileNotification \
+  // Create the buckets
+  gsutil mb -p mlab-oti archive-mlab-oti
+  gsutil mb -p mlab-oti scraper-mlab-oti
+  gsutil mb -p mlab-oti functions-mlab-oti
+  // Deploy the functions.
+  gcloud beta functions deploy transferOnFileNotification \
     --stage-bucket=functions-mlab-oti \
     --trigger-bucket=scraper-mlab-oti \
     --project=mlab-oti
- */
 
-var sys = require('sys');
-var exec = require('child_process').exec;
+
+    CURRENTLY BROKEN
+    Things to try?
+    GOOGLE_APPLICATION_CREDENTIALS ?
+
+ */
 
 var google = require('googleapis');
 
@@ -20,47 +28,88 @@ exports.fileIsProcessable = function (file) {
 };
 
 /**
- * Executes a lambda with auth.
+ * Executes a function with default auth.
  *
  * @param {function} func The function to invoke with auth.
  * @param {function} fail The function to call to indicate failure.
  */
 exports.executeWithAuth = function (func, fail) {
-    // If you want things to be authenticated, then put them inside the callback
-    // here.
     google.auth.getApplicationDefault(
         function (err, authClient, projectId) {
+            console.log('inside auth, projectId = ', projectId)
             if (err) {
                 fail();
                 return;
             }
 
+
+            if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+                // This isn't actually executing.
+                console.log('createScopedRequired')
+                authClient = authClient.createScoped(
+                    ['https://www.googleapis.com/auth/cloud-platform']);
+            }
+            console.log(authClient)
+
+            console.log('executing func')
             func(authClient, projectId);
         }
     );
 };
 
-var puts = function(error, stdout, stderr) { sys.puts(stdout); };
-
 /**
- * Moves a file object from current location to archive-mlab-oti bucket.
+ * Executes a lambda with auth.
  *
- * @param {object} file The file to move.
+ * @param {file} file The object to move.
  * @param {function} done The callback function to indicate cloud function is
  *                        complete.
  */
-exports.moveFile = function (file, done) {
-    // TODO move the file.
-    var gsFilename = 'gs://' + file.bucket + '/' + file.name;
-    var gsDest = 'gs://archive-mlab-oti'
-    child = exec('gsutil mv ' + gsFilename + ' ' + gsDest,
-        function (err, stdout, stderr) {
-            if (err !== null) {
-                console.log('gsutil error: ' + err);
-            }
-            done();
+exports.makeMoveWithAuth = function (file, done) {
+    return function (authClient, projectId) {
+
+        console.log('google.storage')
+        // Doesn't seem to matter whether we do this, or pass the
+        // authClient/project in the storage.objects.copy.
+        var storage = google.storage(
+            {"version": "v1", "auth": authClient, "project": projectId});
+        //var storage = google.storage('v1')
+
+        console.log(storage)
+        console.log(file.name)
+
+        // This is consistently failing.
+        // The same function, invoked through the cloud API explorer,
+        // seems to work just fine, with either cloud-platform, or
+        // devstorage.read-write scope.
+        console.log('storage.objects.copy')
+        storage.objects.copy({
+          //  "auth": authClient,
+          //  "project": projectId,
+            "sourceBucket": file.bucket,
+            "sourceObject": file.name,
+            //"destinationBucket": "foobar-mlab-oti",
+            "destinationBucket": "destination-mlab-sandbox",
+            "destinationObject": file.name
+        },
+      /*  storage.objects.list({
+            "auth": authClient,
+            "project": projectId,
+            "bucket": "scraper-mlab-oti",
+            "prefix": "test/"
+        }, */
+
+        function(err, msg, incoming) {
+            console.log(err)
+            console.log(msg)
+            console.log(incoming)
+            console.log('calling done')
+            done(err, msg, incoming)
         });
-};
+  }
+}
+
+var puts = function(error, stdout, stderr) { sys.puts(stdout); };
+
 
 /**
  * Determines whether a file object should be embargoed, or transfered
@@ -74,20 +123,19 @@ exports.shouldEmbargo = function (file) {
 
 /**
  * Cloud Function to be triggered by Cloud Storage,
- * moves the file to the
- * archive-mlab-oti bucket.
+ * moves the file to the archive-mlab-oti bucket.
  *
  * @param {object} event The Cloud Functions event.
  * @param {function} done The callback function to indicate function complete.
  */
-exports.fileNotification = function fileNotification (event, done) {
+exports.transferOnFileNotification = function transferOnFileNotification (event, done) {
     const file = event.data;
 
     if (exports.fileIsProcessable(file)) {
         if (exports.shouldEmbargo()) {
             // TODO - notify the embargo system.
         } else {
-            exports.moveFile(file, done);
+            exports.executeWithAuth(exports.makeMoveWithAuth(file, done));
         }
     } else {
       done();
