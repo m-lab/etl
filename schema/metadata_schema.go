8f2f6093a49d366aac6ce386c1e22939a23cbf92
Joseph Marques <josephmarques@josephmarques.nyc.corp.google.com>
2017-08-14 15:33:03 -0400

    First attempt at making the pt annotator work. No tests yet.

--- schema/metadata_schema.go
+++ schema/metadata_schema.go
@@ -5,17 +5,18 @@ package schema
 // capitalized for exporting, although the originals in the DB schema
 // are not.
 type GeolocationIP struct {
-	Continent_code string  // Gives a shorthand for the continent
-	Country_code   string  // Gives a shorthand for the country
-	Country_code3  string  // Gives a shorthand for the country
-	Country_name   string  // Name of the country
-	Region         string  // Region or State within the country
-	Metro_code     int64   // Metro code within the country
-	City           string  // City within the region
-	Area_code      int64   // Area code, similar to metro code
-	Postal_code    string  // Postal code, again similar to metro
-	Latitude       float64 // Latitude
-	Longitude      float64 // Longitude
+	Continent_code string  `json:"continent_code, string"` // Gives a shorthand for the continent
+	Country_code   string  `json:"country_code, string"`   // Gives a shorthand for the country
+	Country_code3  string  `json:"country_code3, string"`  // Gives a shorthand for the country
+	Country_name   string  `json:"country_name, string"`   // Name of the country
+	Region         string  `json:"region, string"`         // Region or State within the country
+	Metro_code     int64   `json:"metro_code, integer"`    // Metro code within the country
+	City           string  `json:"city, string"`           // City within the region
+	Area_code      int64   `json:"area_code, integer"`     // Area code, similar to metro code
+	Postal_code    string  `json:"postal_code, string"`    // Postal code, again similar to metro
+	Latitude       float64 `json:"latitude, float"`        // Latitude
+	Longitude      float64 `json:"longitude, float"`       // Longitude
+
 }
 
 // The struct that will hold the IP/ASN data when it gets added to the
