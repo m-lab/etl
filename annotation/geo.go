84f04935e7933727c0106df90bf24ef2e8be4e85
Greg Russell <gfr10598@gmail.com>
2017-10-02 16:12:02 -0400

    Merge _structs into annotation.go

--- annotation/geo.go
+++ annotation/geo.go
@@ -37,6 +37,45 @@ func EnableAnnotation() {
 	getFlagValues()
 }
 
+// The GeolocationIP struct contains all the information needed for the
+// geolocation data that will be inserted into big query. The fiels are
+// capitalized for exporting, although the originals in the DB schema
+// are not.
+type GeolocationIP struct {
+	Continent_code string  `json:"continent_code, string,omitempty"` // Gives a shorthand for the continent
+	Country_code   string  `json:"country_code, string,omitempty"`   // Gives a shorthand for the country
+	Country_code3  string  `json:"country_code3, string,omitempty"`  // Gives a shorthand for the country
+	Country_name   string  `json:"country_name, string,omitempty"`   // Name of the country
+	Region         string  `json:"region, string,omitempty"`         // Region or State within the country
+	Metro_code     int64   `json:"metro_code, integer,omitempty"`    // Metro code within the country
+	City           string  `json:"city, string,omitempty"`           // City within the region
+	Area_code      int64   `json:"area_code, integer,omitempty"`     // Area code, similar to metro code
+	Postal_code    string  `json:"postal_code, string,omitempty"`    // Postal code, again similar to metro
+	Latitude       float64 `json:"latitude, float"`                  // Latitude
+	Longitude      float64 `json:"longitude, float"`                 // Longitude
+
+}
+
+// The struct that will hold the IP/ASN data when it gets added to the
+// schema. Currently empty and unused.
+type IPASNData struct{}
+
+// The main struct for the geo metadata, which holds pointers to the
+// Geolocation data and the IP/ASN data. This is what we parse the JSON
+// response from the annotator into.
+type GeoData struct {
+	Geo *GeolocationIP // Holds the geolocation data
+	ASN *IPASNData     // Holds the IP/ASN data
+}
+
+// The RequestData schema is the schema for the json that we will send
+// down the pipe to the annotation service.
+type RequestData struct {
+	IP        string    // Holds the IP from an incoming request
+	IPFormat  int       // Holds the ip format, 4 or 6
+	Timestamp time.Time // Holds the timestamp from an incoming request
+}
+
 // TODO(JosephMarques) See if there is a better way of determining
 // where to send the request (there almost certainly is)
 var AnnotatorURL = "https://annotator-dot-" +
