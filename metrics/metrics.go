01a991dee893a7fbd1dc62bc11c38ea278f13f94
yachang <changya@gmail.com>
2017-10-09 11:02:26 -0400

    update counter

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -73,20 +73,22 @@ var (
 	//    etl_annotator_Error_Count
 	// Example usage:
 	//    metrics.AnnotationErrorCount.Inc()
-	AnnotationErrorCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "etl_annotator_Error_Count",
-		Help: "The current number of errors encountered while attempting to add geo data.",
-	}, []string{"source"})
+	AnnotationErrorCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "etl_annotator_Error_Count",
+			Help: "The current number of errors encountered while attempting to add geo data.",
+		}, []string{"source"})
 
 	// Measures the number of annotation warnings
 	// Provides metrics:
 	//    etl_annotator_Warning_Count
 	// Example usage:
 	//    metrics.AnnotationWarningCount.Inc()
-	AnnotationWarningCount = prometheus.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "etl_annotator_Warning_Count",
-		Help: "The current number of Warnings encountered while attempting to add geo data.",
-	}, []string{"source"})
+	AnnotationWarningCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "etl_annotator_Warning_Count",
+			Help: "The current number of Warnings encountered while attempting to add geo data.",
+		}, []string{"source"})
 
 	// Counts the number of tasks processed by the pipeline.
 	//
