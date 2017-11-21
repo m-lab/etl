f6453afb56ff3e3cd6080199195b8a369592ec28
Greg Russell <gfr10598@gmail.com>
2017-11-21 13:35:31 -0500

    various tweaks

--- legacy/convert-year.sh
+++ legacy/convert-year.sh
@@ -5,5 +5,5 @@ for month in $(seq -w 01 12);
   #echo processing month $year$month
   do for day in $(seq -w 01 31); do ./convert-legacy-to-common.sh $year-$month-$day &  done;
   wait
-  echo completed month $year$mont
+  echo completed month $year$month
 done;
