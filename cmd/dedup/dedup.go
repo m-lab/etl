6e9d7f500f891f3ba9fa425454bc0e540a3f67b0
Greg Russell <gfr10598@gmail.com>
2017-12-15 09:08:23 -0500

    flesh out command line dedup

--- cmd/dedup/dedup.go
+++ cmd/dedup/dedup.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/m-lab/etl/dedup"
@@ -13,25 +14,36 @@ import (
 var (
 	// TODO - replace this with a service account?
 	fProject          = flag.String("project", "", "BigQuery project.")
-	fTemplatePrefix   = flag.String("template_prefix", "", "table prefix")
+	fTemplatePrefix   = flag.String("template_prefix", "", "source dataset.table_prefix")
 	fDelay            = flag.Float64("delay", 48, "delay (hours) from last update")
-	fDestinationTable = flag.String("destination_table", "etl.dest", "destination table")
+	fDestinationTable = flag.String("destination_table", "etl.dest", "destination dataset.table")
 	fDeleteAfterCopy  = flag.Bool("delete", false, "Should delete table after copy")
 	fDryRun           = flag.Bool("dry_run", false, "Print actions instead of executing")
 )
 
 func main() {
 	flag.Parse()
-	log.Println(*fProject)
-	dsExt, err := bqext.NewDataset(*fProject, "batch")
 
-	if *fTemplatePrefix == "" {
-		log.Println("template_prefix must be non-empty")
+	src := strings.Split(*fTemplatePrefix, ".")
+	if len(src) != 2 {
+		log.Println("template_prefix must have dataset.table_prefix")
 		os.Exit(1)
 	}
-	err = dedup.ProcessTablesMatching(&dsExt, *fTemplatePrefix, "public", "ndt", 14*24*time.Hour)
+	dsExt, err := bqext.NewDataset(*fProject, src[0])
 	if err != nil {
 		log.Println(err)
+		os.Exit(1)
 	}
 
+	dest := strings.Split(*fDestinationTable, ".")
+	if len(dest) != 2 {
+		log.Println("template_prefix must have dataset.table_prefix")
+		os.Exit(1)
+	}
+	// TODO fix delay param.
+	err = dedup.ProcessTablesMatching(&dsExt, src[1], dest[0], dest[1], 14*24*time.Hour)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
