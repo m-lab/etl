5a54c46b300616539d54d5b9d9ee223662653e7f
Greg Russell <gfr10598@gmail.com>
2017-12-20 20:24:06 -0500

    starting conversion to legacysql

---
+++ schema/views/ndt_exhaustive_legacysql.sql
@@ -0,0 +1,9 @@
+#legacySQL
+-- Combined view of plx legacy fast table, up to May 10, and
+-- new ETL table, from May 10, 2017 onward.
+-- Includes blacklisted and EB tests, which should be removed before analysis.
+-- Note that at present, data from May 10 to mid September does NOT have geo annotations.
+SELECT *
+FROM
+ [${DATASET}.common_etl_legacysql],
+ [measurement-lab:legacy.ndt_plx_legacysql]
