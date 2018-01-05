06026b939dac5a456c54e4258c1d94e6c664cac6
Greg Russell <gfr10598@gmail.com>
2018-01-05 13:41:45 -0500

    document and check for parameters

--- add-tags.sh
+++ add-tags.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# This script substitutes TRAVIS env variables for placeholders in deployment yaml files.
+# It overwrites the specified yaml file with the substituted variables.
+#
+# Example:
+#   ./app-tags.sh cmd/etl_worker app-ndt.yaml
+
+BASEDIR=${1:?Please provide the base directory containing yaml file}
+YAML=${2:?Please provide yaml file name, e.g. app-ndt.yaml}
 
 set -e
 set -x
 set -u
-
 pushd $TRAVIS_BUILD_DIR/cmd/etl_worker
-cat $1 | sed "s/__COMMIT_HASH__/$TRAVIS_COMMIT/" | sed "s/__RELEASE_TAG__/$TRAVIS_TAG/" > $2
-cat $2
+yaml_text =`cat $YAML`
+echo $yaml_text | sed "s/__COMMIT_HASH__/$TRAVIS_COMMIT/" | sed "s/__RELEASE_TAG__/$TRAVIS_TAG/" > $YAML
+cat $YAML
 popd
