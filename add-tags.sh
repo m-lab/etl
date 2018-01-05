be4f0f89649bdd39ed2602b1a9a16efd002801ea
Greg Russell <gfr10598@gmail.com>
2018-01-05 13:23:06 -0500

    substitute

--- add-tags.sh
+++ add-tags.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-echo $1
+set -e
+set -x
+set -u
+
+pushd $TRAVIS_BUILD_DIR/cmd/etl_worker
+cat $1 | sed 's/__COMMIT_HASH__/$TRAVIS_COMMIT/' | sed 's/__RELEASE_TAG__/$TRAVIS_TAG/' | $2
+cat $2
+popd
+
+echo $2
