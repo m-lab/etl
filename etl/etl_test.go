2dc7ea51608be2a82946a80525fd2f5b35c179bd
yachang <changya@gmail.com>
2018-01-11 14:19:16 -0500

    minor

--- etl/etl_test.go
+++ etl/etl_test.go
@@ -64,7 +64,6 @@ func TestCalculateIPDistance(t *testing.T) {
 	}
 	dist2, _ := etl.CalculateIPDistance("2001:0db8:85a3:0000:0000:8a2e:0370:7334", "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
 	if dist2 != 128 {
-		fmt.Println(dist2)
 		t.Errorf("Error in calculating IPv6 distance!\n")
 		return
 	}
