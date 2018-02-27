27b755abdf9b79409e83db21f5ae00aa9ee58d1b
yachang <yachang@users.noreply.github.com>
2018-02-27 10:41:37 -0500

    Update globals.go

--- etl/globals.go
+++ etl/globals.go
@@ -210,7 +210,7 @@ var (
 		NDT:             10,
 		NDT_OMIT_DELTAS: 50,
 		SS:              100,
-		PT:              1000,
+		PT:              300,
 		SW:              100,
 		INVALID:         0,
 	}
