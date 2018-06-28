84b0d4c9a0e21ef89678215d9b2346cf4f2d2611
Stephen Soltesz <soltesz@google.com>
2018-06-28 12:32:41 -0400

    Add parserVersion column

--- schema/switch_schema.go
+++ schema/switch_schema.go
@@ -8,14 +8,15 @@ type Sample struct {
 
 // SwitchStats represents a row of data taken from the raw DISCO export file.
 type SwitchStats struct {
-	TaskFilename string   `json:"task_filename,string" bigquery:"task_filename"`
-	TestID       string   `json:"test_id,string" bigquery:"test_id"`
-	ParseTime    int64    `json:"parse_time,int64" bigquery:"parse_time"`
-	LogTime      int64    `json:"log_time,int64" bigquery:"log_time"`
-	Sample       []Sample `json:"sample" bigquery:"sample"`
-	Metric       string   `json:"metric" bigquery:"metric"`
-	Hostname     string   `json:"hostname" bigquery:"hostname"`
-	Experiment   string   `json:"experiment" bigquery:"experiment"`
+	TaskFilename  string   `json:"task_filename,string" bigquery:"task_filename"`
+	TestID        string   `json:"test_id,string" bigquery:"test_id"`
+	ParseTime     int64    `json:"parse_time,int64" bigquery:"parse_time"`
+	ParserVersion string   `json:"parser_version,string" bigquery:"parser_version"`
+	LogTime       int64    `json:"log_time,int64" bigquery:"log_time"`
+	Sample        []Sample `json:"sample" bigquery:"sample"`
+	Metric        string   `json:"metric" bigquery:"metric"`
+	Hostname      string   `json:"hostname" bigquery:"hostname"`
+	Experiment    string   `json:"experiment" bigquery:"experiment"`
 }
 
 // Size estimates the number of bytes in the SwitchStats object.
