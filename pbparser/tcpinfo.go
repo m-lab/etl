340498e8a9eae6d726dcdb195822bc18b29bc0c4
Greg Russell <gfr10598@gmail.com>
2018-07-23 11:42:57 -0400

    Add pbparser package

---
+++ pbparser/tcpinfo.go
@@ -0,0 +1,4 @@
+// Package pbparser comprises parsers that use protobufs in one way or another.
+// This is separate from other parsers, because protobuffers incur additional build
+// dependencies and overhead.
+package pbparser
