36198a36af777740e5c5ae8a55fd2e70f918b2fa
Greg Russell <gfr10598@gmail.com>
2018-07-23 14:15:23 -0400

    parse entire file

--- pbparser/reader_test.go
+++ pbparser/reader_test.go
@@ -1,16 +1,11 @@
 package pbparser_test
 
 import (
-	"bufio"
-	"encoding/binary"
-	"io"
 	"log"
 	"testing"
 
-	"github.com/gogo/protobuf/proto"
+	"github.com/m-lab/etl/pbparser"
 	"github.com/m-lab/tcp-info/zstd"
-
-	tcpinfo "github.com/m-lab/tcp-info/nl-proto"
 )
 
 func init() {
@@ -18,31 +13,24 @@ func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-func TestReader(t *testing.T) {
+func TestProtoParsing(t *testing.T) {
 	// Cache info new 140  err 0 same 277 local 789 diff 3 total 1209
 	// 1209 sockets 143 remotes 403 per iteration
 	//source := "testdata/20180717Z144141.694U00148024L100.101.230.223:41506R192.30.253.116:443_00000.zst"
 	source := "testdata/20180607Z153856.193U00000000L2620:0:1003:415:b33e:9d6a:81bf:87a1:36032R2607:f8b0:400d:c0d::81:5034_00000.zst"
 	log.Println("Reading messages from", source)
 	rdr := zstd.NewReader(source)
-	byteRdr := bufio.NewReader(rdr)
 
-	size, err := binary.ReadUvarint(byteRdr)
+	protos, err := pbparser.ReadAll(rdr)
 
 	if err != nil {
 		t.Fatal(err)
 	}
-	log.Println(size)
-	buf := make([]byte, size)
-	n, err := io.ReadFull(byteRdr, buf)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if n != len(buf) {
-		t.Fatalf("wrong size %d != %d", n, len(buf))
-	}
 
-	pb := tcpinfo.TCPDiagnosticsProto{}
-	proto.Unmarshal(buf, &pb)
-	log.Println(pb)
+	if len(protos) != 17 {
+		t.Error("Should be 17 messages", len(protos))
+	}
+	for i := range protos {
+		log.Println(protos[i])
+	}
 }
--- pbparser/tcpinfo.go
+++ pbparser/tcpinfo.go
@@ -2,3 +2,47 @@
 // This is separate from other parsers, because protobuffers incur additional build
 // dependencies and overhead.
 package pbparser
+
+import (
+	"bufio"
+	"encoding/binary"
+	"errors"
+	"io"
+	"log"
+
+	"github.com/gogo/protobuf/proto"
+
+	tcpinfo "github.com/m-lab/tcp-info/nl-proto"
+)
+
+// ReadAll reads and marshals all protobufs from a Reader.
+func ReadAll(rdr io.Reader) ([]tcpinfo.TCPDiagnosticsProto, error) {
+	var result []tcpinfo.TCPDiagnosticsProto
+
+	byteRdr := bufio.NewReader(rdr)
+
+	for {
+		size, err := binary.ReadUvarint(byteRdr)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			return nil, err
+		}
+		log.Println(size)
+		buf := make([]byte, size)
+		n, err := io.ReadFull(byteRdr, buf)
+		if err != nil {
+			return nil, err
+		}
+		if n != len(buf) {
+			return nil, errors.New("corrupted protobuf file")
+		}
+
+		pb := tcpinfo.TCPDiagnosticsProto{}
+		proto.Unmarshal(buf, &pb)
+		result = append(result, pb)
+	}
+
+	return result, nil
+}
