af2fc7c78013e37a17a8a960f7a365a0c1ea1dec
Greg Russell <gfr10598@gmail.com>
2019-05-24 12:52:27 -0400

    add annotatable interface to tcpinfo

--- parser/tcpinfo.go
+++ parser/tcpinfo.go
@@ -39,9 +39,10 @@ import (
 	"github.com/m-lab/tcp-info/snapshot"
 )
 
+// TODO move to schema/tcpinfo.go
 type ServerInfo struct {
 	IP   string
-	Port string
+	Port uint16
 	IATA string
 
 	Geo     *api.GeolocationIP
@@ -50,7 +51,7 @@ type ServerInfo struct {
 
 type ClientInfo struct {
 	IP   string
-	Port string
+	Port uint16
 
 	Geo     *api.GeolocationIP
 	Network *api.ASData // NOTE: dominant ASN is available at top level.
@@ -66,8 +67,8 @@ type TCPRow struct {
 	UUID     string    // Top level just because
 	TestTime time.Time // Must be top level for partitioning
 
-	ClientASN int32 // Top level for clustering
-	ServerASN int32 // Top level for clustering
+	ClientASN uint32 // Top level for clustering
+	ServerASN uint32 // Top level for clustering
 
 	ParseInfo *ParseInfo
 
@@ -79,6 +80,85 @@ type TCPRow struct {
 	Snapshots []*snapshot.Snapshot
 }
 
+// Implement parser.Annotatable
+
+// GetLogTime returns the timestamp that should be used for annotation.
+func (row *TCPRow) GetLogTime() time.Time {
+	return row.TestTime
+}
+
+// GetClientIPs returns the client (remote) IP for annotation.  See parser.Annotatable
+func (row *TCPRow) GetClientIPs() []string {
+	if row.Client == nil {
+		snap := row.FinalSnapshot
+		if snap == nil || snap.InetDiagMsg == nil {
+			log.Println("No Snapshot")
+			return []string{}
+		}
+		id := snap.InetDiagMsg.ID
+		row.Client = &ClientInfo{IP: id.DstIP().String(), Port: id.DPort()}
+	}
+
+	return []string{row.Client.IP}
+}
+
+// GetServerIP returns the server (local) IP for annotation.  See parser.Annotatable
+func (row *TCPRow) GetServerIP() string {
+	if row.Server == nil {
+		snap := row.FinalSnapshot
+		if snap == nil || snap.InetDiagMsg == nil {
+			log.Println("No Snapshot")
+			return ""
+		}
+		id := snap.InetDiagMsg.ID
+		row.Server = &ServerInfo{IP: id.SrcIP().String(), Port: id.SPort()}
+	}
+
+	if row.Server != nil {
+		return row.Server.IP
+	}
+	log.Println("nil server for row")
+	return row.Server.IP
+}
+
+// AnnotateClients adds the client annotations. See parser.Annotatable
+func (row *TCPRow) AnnotateClients(annMap map[string]*api.Annotations) error {
+	if annMap != nil {
+		ann, ok := annMap[row.Client.IP]
+		if ok && ann.Geo != nil {
+			row.Client.Geo = ann.Geo
+		}
+		if ann.Network != nil {
+			row.Client.Network = ann.Network
+			asn, err := ann.Network.BestASN()
+			if err != nil {
+				log.Println(err)
+			} else {
+
+				row.ClientASN = uint32(asn)
+			}
+		}
+	}
+	return nil
+}
+
+// AnnotateServer adds the server annotations. See parser.Annotatable
+func (row *TCPRow) AnnotateServer(local *api.Annotations) error {
+	row.Client.Geo = local.Geo
+	if local.Network != nil {
+		row.Server.Network = local.Network
+		asn, err := local.Network.BestASN()
+		if err != nil {
+			log.Println(err)
+		} else {
+
+			row.ServerASN = uint32(asn)
+		}
+	}
+	return nil
+}
+
+// Schema returns the Bigquery schema for TCPRow.
 func (row *TCPRow) Schema() (bigquery.Schema, error) {
 	sch, err := bigquery.InferSchema(row)
 	if err != nil {
