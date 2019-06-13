95dbfeaa094b84bc056cfd79b9a56d7c969820a7
yachang <yachang@users.noreply.github.com>
2019-06-13 14:59:36 -0400

    Add new schema for PT (#688)
    * add new schema for PT.
    
    * more
    
    * uuid

--- schema/pt_schema.go
+++ schema/pt_schema.go
@@ -7,7 +7,7 @@ import (
 	"github.com/m-lab/annotation-service/api"
 )
 
-// TODO(dev): use mixed case Go variable names throughout
+// TODO: The following schema will be deprecated soon.
 
 type ParisTracerouteHop struct {
 	Protocol         string            `json:"protocal,string"`
@@ -42,4 +42,43 @@ type PT struct {
 	Connection_spec      MLabConnectionSpecification `json:"connection_spec"`
 	Paris_traceroute_hop ParisTracerouteHop          `json:"paris_traceroute_hop"`
 	Type                 int32                       `json:"type,int32"`
+}
+
+// This will be new schema for traceroute tests.
+
+type HopIP struct {
+	Ip          string `json:"ip,string"`
+	City        string `json:"city,string"`
+	CountryCode string `json:"country_code,string"`
+	Hostname    string `json:"hostname,string"`
+}
+
+type HopProbe struct {
+	Flowid int64     `json:"flowid,int64"`
+	Rtt    []float64 `json:"rtt"`
+}
+
+type HopLink struct {
+	HopDstIp string     `json:"hop_dst_ip,string"`
+	TTL      int64      `json:"ttl,int64"`
+	Probes   []HopProbe `json:"probes"`
+}
+
+type ScamperHop struct {
+	Source HopIP     `json:"source"`
+	Linkc  int64     `json:"linkc,int64"`
+	Links  []HopLink `json:"link"`
+}
+
+type PTTest struct {
+	UUID           string       `json:"uuid,string" bigquery:"uuid"`
+	Parseinfo      ParseInfo    `json:"parseinfo"`
+	StartTime      int64        `json:"start_time,int64" bigquery:"start_time"`
+	StopTime       int64        `json:"stop_time,int64" bigquery:"stop_time"`
+	ScamperVersion string       `json:"scamper_version,string" bigquery:"scamper_version"`
+	Source         ServerInfo   `json:"source"`
+	Destination    ClientInfo   `json:"destination"`
+	ProbeSize      int64        `json:"probe_size,int64"`
+	ProbeC         int64        `json:"probec,int64"`
+	Hop            []ScamperHop `json:"hop"`
 }
