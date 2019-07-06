3d6003d7710760894b54055e121f11995d66ff56
Gregory Russell <gfr@google.com>
2019-07-06 11:21:12 -0400

    flesh out doneHandler

--- active/active.go
+++ active/active.go
@@ -6,6 +6,14 @@ package active
 // in a gs:// prefix.  It processes the files in lexographical order, and maintains
 // status info in datastore.
 
+// Design:
+//  1. a token channel is passed in to ProcessAll, and used to determine how many tasks may
+//     be in flight.  It is returned to the caller when there are no more tasks to start,
+//     but there may still be tasks running, and tokens that will be returned later.
+//  2. a doneHandler waits for task completions, and updates the state.  It starts additional
+//     tasks if there are any.  When there are no more tasks, it signals ProcessAll
+//     that the token channel may be returned to the caller.
+
 // TODO: Utilization based management:
 // The manager start new tasks when either:
 //   1. Two tasks have completed since the last task started.
@@ -15,12 +23,14 @@ package active
 import (
 	"context"
 	"errors"
+	"log"
 	"sync"
 	"time"
-	"log"
+
+	"cloud.google.com/go/storage"
+	"github.com/m-lab/etl/cloud/gcs"
 
 	"github.com/GoogleCloudPlatform/google-cloud-go-testing/storage/stiface"
-	"github.com/m-lab/etl/cloud/gcs"
 
 	"github.com/m-lab/etl/worker"
 )
@@ -40,7 +50,9 @@ type DatastoreRecord struct {
 // These are NOT thread-safe, and should only be read and modified by
 // a single goroutine.
 type TaskFile struct {
-	path string
+	// TODO - is this needed?
+	path string               // Full path to object.
+	obj  *storage.ObjectAttrs // Optional if completed or errored.
 
 	// Status - pending, processing, completed, errored.
 	status string // For now - eventually this might be an enum.
@@ -52,54 +64,86 @@ type TaskFile struct {
 // FileSource handles reading, caching, and updating a list of files,
 // and tracking the processing status of each file.
 type FileSource struct {
-	mutex  sync.Mutex
-	project string
-	prefix string
+	client     stiface.Client
+	project    string
+	prefix     string
+	retryLimit int // number of retries for failed tasks.
 
-	pending   []*TaskFile // Ordered list
-	inFlight  map[string]*TaskFile
-	completed []*TaskFile // Ordered list.
-	retry     []*TaskFile
+	// Channel to handle cleanup when a task is completed.
+	done chan *TaskFile
 
-	retries int // number of retries for failed tasks.
+	// Remaining fields should only be accessed by the ProcessAll function.
+	// while holding the mutex.
+	status DatastoreRecord
 
-	// Semaphore used to control number of active tasks.
-	sem chan struct{}
+	lastUpdate time.Time // Time of last call to UpdatePending
+	allFiles   map[string]*TaskFile
+	pending    []*TaskFile // Ordered list
+	inFlight   map[string]*TaskFile
 }
 
-func NewFileSource(sem chan struct{}, sc stiface.Client, project string, prefix string) *FileSource {
-	pending, _, err := gcs.GetAllFiles(context.Background(), sc, project, prefix)
+// NewFileSource creates a new source for active processing.
+func NewFileSource(sc stiface.Client, project string, prefix string, retryLimit int) (*FileSource, error) {
+	files, _, err := gcs.GetFilesSince(context.Background(), sc, project, prefix, time.Time{})
 	if err != nil {
 		// TODO metric
-		log.Println(err)
-		return nil
+		return nil, err
 	}
 
-	return &FileSource{status: DatastoreRecord{}, 
-	pending: pending, inFlight: map[string]*TaskFile, len(sem),
-	sem: sem}
+	if len(files) < 1 {
+		return nil, ErrNoMoreFiles
+	}
+
+	fs := FileSource{
+		client:     sc,
+		project:    project,
+		prefix:     prefix,
+		status:     DatastoreRecord{Name: prefix},
+		pending:    make([]*TaskFile, 0, len(files)),
+		inFlight:   make(map[string]*TaskFile, 100),
+		retryLimit: retryLimit,
+		done:       make(chan *TaskFile, 0)}
+
+	// Don't need to hold the lock yet, because we are sole owner.
+	for _, f := range files {
+		fs.pending = append(fs.pending, &TaskFile{path: f.Name, obj: f})
+	}
+	return &fs, nil
 }
 
-// UpdatePending should be called when there are no more pending tasks.
-func (fs *FileSource) UpdatePending(ctx context.Context) error {
-	fs.mutex.Lock()
-	defer fs.mutex.Unlock()
+// updatePending should be called when there are no more pending tasks.
+// Not thread-safe - should only be called by ProcessAll.
+func (fs *FileSource) updatePending(ctx context.Context) error {
+	updateTime := time.Now().Add(-time.Second)
+	files, _, err := gcs.GetFilesSince(context.Background(), fs.client, fs.project, fs.prefix, fs.lastUpdate)
+	if err != nil {
+		return err
+	}
+	fs.lastUpdate = updateTime
 
-	// Can we use mtime to streamline update?
-	// TODO
+	for _, f := range files {
+		if f.Prefix != "" {
+			log.Println("Skipping subdirectory:", f.Prefix)
+			continue // skip directories
+		}
+		// Append any new files that aren't found in existing Taskfiles.
+		if _, exists := fs.allFiles[f.Name]; !exists {
+			tf := TaskFile{path: f.Name, obj: f}
+			fs.allFiles[f.Name] = &tf
+			fs.pending = append(fs.pending, &tf)
+		}
+	}
 
 	return nil
 }
 
-// Next returns the next pending TaskFile.  It runs Update if there
+// next returns the next pending TaskFile.  It runs Update if there
 // are initially none available, and reprocesses tasks from the
 // errored list if the there are still none pending.
 // Caller should have already obtained a semaphore.
-func (fs *FileSource) Next() (*TaskFile, error) {
-	fs.mutex.Lock()
-	defer fs.mutex.Unlock()
-	if len(fs.pending) < 1 {
-		err := fs.UpdatePending(context.Background())
+func (fs *FileSource) next(ctx context.Context) (*TaskFile, error) {
+	if len(fs.pending) == 0 {
+		err := fs.updatePending(ctx)
 		if err != nil {
 			return nil, err
 		}
@@ -109,25 +153,14 @@ func (fs *FileSource) Next() (*TaskFile, error) {
 		fs.pending = fs.pending[1:]
 		return tf, nil
 	}
-	if len(fs.retry) > 0 {
-		tf := fs.retry[0]
-		fs.retry = fs.retry[1:]
-		return tf, nil
-	}
 	return nil, nil
 }
 
-// CompleteTask updates the FileSource to reflect the completion of
+// complete updates the FileSource to reflect the completion of
 // a processing attempt.
 // If the processing ends in an error, the task will be moved to
-// the retry list, unless the task has already been retried fs.retry times.
-func (fs *FileSource) CompleteTask(tf *TaskFile) error {
-	fs.mutex.Lock()
-	defer fs.mutex.Unlock()
-	defer func() {
-		fs.sem <- struct{}{}
-	}()
-
+// the end of the pending list, unless the task has already been retried fs.retry times.
+func (fs *FileSource) complete(tf *TaskFile) error {
 	_, exists := fs.inFlight[tf.path]
 	if !exists {
 		return errors.New("task not in flight")
@@ -135,46 +168,76 @@ func (fs *FileSource) CompleteTask(tf *TaskFile) error {
 
 	delete(fs.inFlight, tf.path)
 	if tf.lastErr != nil {
-		if tf.failures < 1 {
-			fs.retry = append(fs.retry, tf)
+		if tf.failures < fs.retryLimit {
+			fs.pending = append(fs.pending, tf)
 			tf.failures++
 		}
-	} else {
-		fs.completed = append(fs.completed, tf)
 	}
 	return nil
 }
 
 var ErrNoMoreFiles = errors.New("No more files in file source")
 
-// Process starts the next task.  For concurrent processing, use go StartNext().
-// This function must NOT hold the mutex, and should call other thread-safe functions
-// for all accesses to FileSource fields.
-func (fs *FileSource) ProcessOne(tf *TaskFile) error {
-	// next is now owned.
+// process starts the next task.
+func (fs *FileSource) process(tf *TaskFile) {
 	_, tf.lastErr = worker.ProcessTask(tf.path)
+	fs.done <- tf
+}
 
-	return fs.CompleteTask(tf) // This also returns the semaphore.
+// doneHandler handles all the done channel returns.  For every
+// completed task, it will attempt to start two tasks, to utilize
+// any additional tokens that become available.
+// When there are no further pending items, it decrements the wait group
+// to indicate to ProcessAll that no more tokens are needed.
+func (fs *FileSource) doneHandler(ctx context.Context, tokens chan struct{}, wg *sync.WaitGroup) {
+DRAIN:
+	for {
+		select {
+		case t := <-fs.done:
+			fs.complete(t)
+			<-tokens // return the token
+		case tokens <- struct{}{}:
+			tf, err := fs.next(ctx)
+			if err != nil {
+				log.Println(err)
+			}
+			if tf != nil {
+				fs.process(tf)
+			} else {
+				// return the token
+				<-tokens
+				break DRAIN
+			}
+		}
+	}
+
+	wg.Done()
+	for t := range fs.done {
+		fs.complete(t)
+		<-tokens // return the token
+		if len(fs.inFlight) == 0 {
+			close(fs.done)
+		}
+	}
 }
 
 // ProcessAll iterates through all the TaskFiles, processing each one.
 // At the end, it will also make one attempt to reprocess any that failed
 // the first time.
-func (fs *FileSource) ProcessAll() (chan struct{}, error) {
-
-	for {
-		next, err := fs.Next()
-		if err != nil {
-			return fs.sem, err
-		}
-		if next == nil {
-			break
-		}
-		// Asynchronously process this task.
-		// The semaphore will be returned when task completes.
-		go fs.ProcessOne(next)
+func (fs *FileSource) ProcessAll(ctx context.Context, tokens chan struct{}) error {
+	if len(fs.pending) == 0 {
+		return nil
 	}
 
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// Handle completed tasks, and start more.
+	go fs.doneHandler(ctx, tokens, &wg)
+
+	// Wait until no more pending.
+	wg.Wait()
+
 	// Return the semaphore queue for some other FileSource to use.
-	return fs.sem, nil
+	return nil
+
 }
