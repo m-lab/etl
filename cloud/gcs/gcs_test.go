d5e2ddbf8183b5adf1f691502b2beb1ae36e56b0
Gregory Russell <gfr@google.com>
2019-07-17 10:28:15 -0400

    add test for GetFilesSince

--- cloud/gcs/gcs_test.go
+++ cloud/gcs/gcs_test.go
@@ -1,10 +1,14 @@
 package gcs_test
 
 import (
+	"context"
 	"log"
 	"testing"
+	"time"
 
+	"cloud.google.com/go/storage"
 	"github.com/m-lab/etl/cloud/gcs"
+	"github.com/m-lab/go/cloudtest"
 )
 
 func init() {
@@ -33,4 +37,29 @@ func TestLegacyPrefix(t *testing.T) {
 		t.Error(pp)
 	}
 
+}
+
+func TestGetFilesSince(t *testing.T) {
+	fc := cloudtest.GCSClient{}
+	fc.AddTestBucket("foobar",
+		cloudtest.BucketHandle{
+			ObjAttrs: []*storage.ObjectAttrs{
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj1", Size: 101, Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj2", Size: 2020, Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/obj3"},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/subdir/obj4", Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "ndt/2019/01/01/subdir/obj5", Updated: time.Now()},
+				&storage.ObjectAttrs{Name: "obj6", Updated: time.Now()},
+			}})
+
+	files, bytes, err := gcs.GetFilesSince(context.Background(), fc, "project", "gs://foobar/ndt/2019/01/01/", time.Now().Add(-time.Minute))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(files) != 2 {
+		t.Error("Expected 2 files, got", len(files))
+	}
+	if bytes != 2121 {
+		t.Error("Expected total 2121 bytes, got", bytes)
+	}
 }
