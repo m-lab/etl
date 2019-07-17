cc3b547c1280219e0fc4ca10d668dadd6e1881f4
Gregory Russell <gfr@google.com>
2019-07-16 22:18:30 -0400

    Add gcs utility functions

---
+++ cloud/gcs/gcs.go
@@ -0,0 +1,156 @@
+package gcs
+
+import (
+	"context"
+	"errors"
+	"log"
+	"regexp"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/GoogleCloudPlatform/google-cloud-go-testing/storage/stiface"
+	"google.golang.org/api/iterator"
+)
+
+// getFilesAfter returns list of all normal file objects with prefix and mTime > after.
+// returns (objects, byteCount, error)
+func getFilesSince(ctx context.Context, bh stiface.BucketHandle, prefix string, since time.Time) ([]*storage.ObjectAttrs, int64, error) {
+	qry := storage.Query{
+		Delimiter: "/", // This prevents traversing subdirectories.
+		Prefix:    prefix,
+	}
+	// TODO - handle timeout errors?
+	// TODO - should we add a deadline?
+	it := bh.Objects(ctx, &qry)
+
+	files := make([]*storage.ObjectAttrs, 0, 1000)
+
+	byteCount := int64(0)
+	gcsErrCount := 0
+	for o, err := it.Next(); err != iterator.Done; o, err = it.Next() {
+		if err != nil {
+			// TODO - should this retry?
+			// log the underlying error, with added context
+			log.Println(err, "when attempting it.Next()")
+			gcsErrCount++
+			if gcsErrCount > 5 {
+				log.Printf("Failed after %d files.\n", len(files))
+				return files, byteCount, err
+			}
+			continue
+		}
+
+		if o.Updated.Before(since) {
+			continue
+		}
+		byteCount += o.Size
+		files = append(files, o)
+	}
+	return files, byteCount, nil
+}
+
+// *******************************************************************
+// Storage Bucket related stuff.
+//  TODO move to another package?
+// *******************************************************************
+
+// getBucket gets a storage bucket.
+// TODO - this is currently duplicated in etl-gardener/state/state.go
+//   opts       - ClientOptions, e.g. credentials, for tests that need to access storage buckets.
+func getBucket(ctx context.Context, sClient stiface.Client, project, bucketName string) (stiface.BucketHandle, error) {
+	bucket := sClient.Bucket(bucketName)
+	if bucket == nil {
+		return nil, errors.New("Nil bucket")
+	}
+	// Check that the bucket is valid, by fetching it's attributes.
+	// Bypass check if we are running travis tests.
+	_, err := bucket.Attrs(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return bucket, nil
+}
+
+// TODO - similar to code in etl-gardener/cloud/tq/tq.go
+func GetFilesSince(ctx context.Context, sClient stiface.Client, project string, prefix string, since time.Time) ([]*storage.ObjectAttrs, int64, error) {
+	// Submit all files from the bucket that match the prefix.
+	p, err := ParsePrefix(prefix)
+	if err != nil {
+		// If there is a parse error, log and skip request.
+		log.Println(err)
+		return nil, 0, err
+	}
+
+	// Use a real storage bucket.
+	// TODO - add a persistent storageClient to the rex object?
+	// TODO - try cancelling the context instead?
+	bucket, err := getBucket(ctx, sClient, project, p.Bucket)
+	if err != nil {
+		// if err == io.EOF && env.TestMode {
+		// 	log.Println("Using fake client, ignoring EOF error")
+		// 	return nil, 0, nil
+		// }
+		log.Println(err)
+		return nil, 0, err
+	}
+
+	return getFilesSince(ctx, bucket, p.Path(), since)
+}
+
+// CODE BELOW is common with etl-gardener state.go.
+
+// These are common with code in etl/etl/globals.go
+const (
+	bucket   = `gs://([^/]*)/`
+	expType  = `(?:([a-z-]+)/)?([a-z-]+)/` // experiment OR experiment/type
+	datePath = `(\d{4}/[01]\d/[0123]\d)/`
+)
+
+// These are here to facilitate use across queue-pusher and parsing components.
+var (
+	// This matches any valid test file name, and some invalid ones.
+	prefixPattern = regexp.MustCompile(bucket + // #1
+		expType + // #2 #3
+		datePath) // #4 - YYYY/MM/DD
+)
+
+// Prefix is a valid gs:// prefix for either legacy or new platform data.
+type Prefix struct {
+	Bucket     string    // the GCS bucket name.
+	Experiment string    // the experiment name
+	DataType   string    // if empty, this is legacy, and DataType is same as Experiment
+	DatePath   string    // the YYYY/MM/DD date path.
+	Date       time.Time // the time.Time corresponding to the datepath.
+}
+
+// Path returns the path within the bucket, not including the leading gs://bucket/
+func (p Prefix) Path() string {
+	if p.Experiment == "" {
+		return p.DataType + "/" + p.DatePath + "/"
+	}
+	return p.Experiment + "/" + p.DataType + "/" + p.DatePath + "/"
+}
+
+// ParsePrefix Parses prefix, returning {bucket, experiment, date string}, error
+// Unless it returns error, the result will be exactly length 3.
+func ParsePrefix(prefix string) (*Prefix, error) {
+	fields := prefixPattern.FindStringSubmatch(prefix)
+
+	if fields == nil {
+		return nil, errors.New("Invalid test path: " + prefix)
+	}
+
+	date, err := time.Parse("2006/01/02", fields[4])
+	if err != nil {
+		return nil, err
+	}
+	p := Prefix{
+		Bucket:     fields[1],
+		Experiment: fields[2],
+		DataType:   fields[3],
+		DatePath:   fields[4],
+		Date:       date,
+	}
+
+	return &p, nil
+}
