30571a9e5dd676590b192b3e657f48eb871c4ce4
Stephen Soltesz <soltesz@google.com>
2019-07-18 17:54:23 -0400

    Update NDTResult names

--- parser/parser.go
+++ parser/parser.go
@@ -43,8 +43,8 @@ func NewParser(dt etl.DataType, ins etl.Inserter) etl.Parser {
 	switch dt {
 	case etl.NDT:
 		return NewNDTParser(ins)
-	case etl.NDT_LEGACY:
-		return NewNDTLegacyParser(ins)
+	case etl.NDT_RESULT:
+		return NewNDTResultParser(ins)
 	case etl.SS:
 		return NewDefaultSSParser(ins) // TODO fix this hack.
 	case etl.PT:
