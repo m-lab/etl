57bec9efcc0979e0699ff0f620e9f526dd994197
Stephen Soltesz <soltesz@google.com>
2019-08-21 11:29:54 -0400

    Add ClientMetadata to BQ schema for NDT result

--- schema/ndt_ndt5.go
+++ schema/ndt_ndt5.go
@@ -2,26 +2,49 @@ package schema
 
 import (
 	"cloud.google.com/go/bigquery"
+	"github.com/kr/pretty"
 	"github.com/m-lab/go/bqx"
-
+	"github.com/m-lab/go/rtx"
 	"github.com/m-lab/ndt-server/data"
 )
 
-// NDT5Result defines the BQ schema for the NDT Result produced by the
-// ndt-server for the NDT5 clients.
-type NDT5Result struct {
+// NameValue is a BigQuery-compatible type for ClientMetadata "name"/"value" pairs.
+type NameValue struct {
+	Name  string
+	Value string
+}
+
+// NDTResult defines the BQ schema for the NDT Result produced by the
+// ndt-server for the NDT clients.
+type NDTResult struct {
 	ParseInfo *ParseInfo
 	TestID    string         `json:"test_id,string" bigquery:"test_id"`
 	LogTime   int64          `json:"log_time,int64" bigquery:"log_time"`
 	Result    data.NDTResult `json:"result" bigquery:"result"`
 }
 
+// A local struct used to infer the schema we wish to append to the
+// result.Control, result.Upload, and result.Download fields.
+type clientMetadata struct {
+	ClientMetadata []NameValue
+}
+
 // Schema returns the BigQuery schema for NDTResult.
-func (row *NDT5Result) Schema() (bigquery.Schema, error) {
+func (row *NDTResult) Schema() (bigquery.Schema, error) {
 	sch, err := bigquery.InferSchema(row)
 	if err != nil {
 		return bigquery.Schema{}, err
 	}
 	rr := bqx.RemoveRequired(sch)
-	return rr, nil
+	var md clientMetadata
+	mdSch, err := bigquery.InferSchema(&md)
+	mdSch = bqx.RemoveRequired(mdSch)
+	c := bqx.CustomizeAppend(rr, map[string]*bigquery.FieldSchema{
+		"Control":  mdSch[0],
+		"Upload":   mdSch[0],
+		"Download": mdSch[0],
+	})
+	pretty.Print(c)
+	rtx.Must(err, "Failed to infer schema for nvArray")
+	return c, err
 }
