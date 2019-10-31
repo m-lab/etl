49ebe39e9ed70d891002a1495bc4c3a8eb0f38ea
Stephen Soltesz <soltesz@google.com>
2019-10-31 18:58:44 -0400

    Use correct command to generate binary

--- schema/schema.go
+++ schema/schema.go
@@ -1,6 +1,6 @@
 package schema
 
 // Requires go-bindata tool in environment:
-//   go get -u github.com/go-bindata/go-bindata
+//   go get -u github.com/go-bindata/go-bindata/go-bindata
 //
 //go:generate go-bindata -pkg schema -nometadata -prefix descriptions descriptions
