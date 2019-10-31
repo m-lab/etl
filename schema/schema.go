a5c6615d1312048a2e7ce102467ba4883a3cd766
Stephen Soltesz <soltesz@google.com>
2019-10-30 21:17:59 -0400

    Actually generate code

--- schema/schema.go
+++ schema/schema.go
@@ -1,10 +1,6 @@
 package schema
 
-// NOTE: assert that the included data and generated data are the same.
-//
-// Update bindata.go using:
+// Requires go-bindata tool in environment:
 //   go get -u github.com/go-bindata/go-bindata
-//   go-bindata -pkg schema -nometadata -prefix descriptions descriptions
 //
-//go:generate go-bindata -pkg schema -nometadata -prefix descriptions -o /tmp/current-bindata.go descriptions
-//go:generate diff -q bindata.go /tmp/current-bindata.go
+//go:generate go-bindata -pkg schema -nometadata -prefix descriptions descriptions
