aa4c689ea9572d82797f62d9c806cf91d6cefd1e
Stephen Soltesz <soltesz@google.com>
2019-10-30 21:06:27 -0400

    Enforce sources match generated bindata.go

--- schema/schema.go
+++ schema/schema.go
@@ -1,3 +1,10 @@
 package schema
 
-//go:generate go-bindata -pkg schema -prefix descriptions descriptions
+// NOTE: assert that the included data and generated data are the same.
+//
+// Update bindata.go using:
+//   go get -u github.com/go-bindata/go-bindata
+//   go-bindata -pkg schema -nometadata -prefix descriptions descriptions
+//
+//go:generate go-bindata -pkg schema -nometadata -prefix descriptions -o /tmp/current-bindata.go descriptions
+//go:generate diff -q bindata.go /tmp/current-bindata.go
