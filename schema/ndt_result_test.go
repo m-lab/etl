d43f822f6e3623703d74508c36003b520322952a
Stephen Soltesz <soltesz@google.com>
2019-11-01 16:56:12 -0400

    Verify that select fields are all checked

--- schema/ndt_result_test.go
+++ schema/ndt_result_test.go
@@ -16,11 +16,17 @@ func TestNDTRow_Schema(t *testing.T) {
 	// The complete schema is large, so verify that field descriptions
 	// are present for select fields.
 	fields := []*bigquery.FieldSchema(got)
+	count := 0
 	for _, field := range fields {
 		for _, name := range []string{"test_id", "ParseInfo", "GitShortCommit"} {
 			if field.Name == name && field.Description == "" {
 				t.Errorf("NDTRow.Schema() missing field.Description for %q", field.Name)
+			} else {
+				count++
 			}
 		}
+	}
+	if count != 3 {
+		t.Errorf("NDTRow.Schema() missing expected fields; got %d, want 3", count)
 	}
 }
