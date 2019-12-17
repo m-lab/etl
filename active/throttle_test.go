3fb77eac4e99270fd1d577aed69fd099216c2083
Gregory Russell <gfr@google.com>
2019-12-17 17:02:09 -0500

    throttle test

--- active/throttle_test.go
+++ active/throttle_test.go
@@ -1,28 +1,52 @@
 package active_test
 
 import (
-	"fmt"
+	"context"
 	"testing"
 
 	"github.com/m-lab/etl/active"
+	"google.golang.org/api/iterator"
 )
 
 type source struct {
 	count int
-	c     counter
+	c     *counter
+}
+
+type tRunnable struct {
+	c *counter
+}
+
+func (tr *tRunnable) Run() error {
+	tr.c.lock.Lock()
+	defer tr.c.lock.Unlock()
+	tr.c.success++
+	return nil
+}
+
+func (tr *tRunnable) Info() string {
+	return "info"
 }
 
 func (s *source) Next(ctx context.Context) (active.Runnable, error) {
-	if s.count < 5 {
-		s.count++
-		return c.toRunnable(), nil
+	if s.count > 0 {
+		s.count--
+		return &tRunnable{s.c}, nil
 	}
 	return nil, iterator.Done
 }
-
-func (s *source) Next() (active.Runnable, error) {
-	return fmt.Sprint(s.count)
+func (s *source) Label() string {
+	return "label"
 }
 
 func TestThrottledSource(t *testing.T) {
+	src := source{5, NewCounter(t)}
+	// throttle to handle two at a time.
+	ts := active.Throttle(&src, active.NewWSTokenSource(2))
+
+	active.RunAll(context.Background(), ts)
+
+	if src.c.success != 5 {
+		t.Error("Should have been 5 runnables", src.c.success)
+	}
 }
