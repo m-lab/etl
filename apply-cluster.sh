4fc8ff079ff1b298205b95fd2e39bedb45f501c2
Gregory Russell <gfr@google.com>
2019-12-30 19:02:13 -0500

    deployment and README updates

--- apply-cluster.sh
+++ apply-cluster.sh
@@ -15,11 +15,13 @@ set -u
 USAGE="PROJECT=<projectid> CLUSTER=<cluster> TRAVIS_TAG=<tag> TRAVIS_COMMIT=<commit> $0"
 PROJECT=${PROJECT:?Please provide project id: $USAGE}
 CLUSTER=${CLUSTER:?Please provide cluster name: $USAGE}
-TRAVIS_TAG=${TRAVIS_TAG:?Please provide travis tag: $USAGE}
+TRAVIS_TAG=${TRAVIS_TAG:-empty_tag}
 TRAVIS_COMMIT=${TRAVIS_COMMIT:?Please provide travis commit: $USAGE}
 
 # Apply templates
 CFG=/tmp/${CLUSTER}-${PROJECT}.yml
+touch ${CFG}
+pwd
 kexpand expand --ignore-missing-keys k8s/${CLUSTER}/*/*.yml \
     --value GCLOUD_PROJECT=${PROJECT} \
     --value RELEASE_TAG=${TRAVIS_TAG} \
