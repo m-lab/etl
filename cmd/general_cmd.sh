f6272f8d0308c0629c8fbdfb6a2e10f3388214fa
yachang <yachang@users.noreply.github.com>
2020-02-11 10:54:22 -0500

    Update general_cmd.sh

--- cmd/general_cmd.sh
+++ cmd/general_cmd.sh
@@ -10,22 +10,10 @@ KEYNAME=${2:?Please provide the service account keyname}
 BASEDIR=${3:?Please provide the path to the command to run}
 CMD=${4:?Please provide the binary name to run}
 
-# Add gcloud to PATH.
-source "${HOME}/google-cloud-sdk/path.bash.inc"
-source $( dirname "${BASH_SOURCE[0]}" )/gcloudlib.sh
-
-# Authenticate all operations using the given service account.
-activate_service_account "${KEYNAME}"
-
 local keyfile=$( mktemp )
 set +x; echo "${!KEYNAME}" > ${keyfile}
 export GOOGLE_APPLICATION_CREDENTIALS=${keyfile}
 
-# For all options see:
-# https://cloud.google.com/sdk/gcloud/reference/config/set
-gcloud config set core/project "${PROJECT}"
-gcloud config set core/disable_prompts true
-gcloud config set core/verbosity info
 
 # Make build artifacts available to docker build.
 pushd "${BASEDIR}"
