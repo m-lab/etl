e0586846a75082b6c1439acefed9d782cb95a631
yachang <yachang@users.noreply.github.com>
2020-02-11 13:05:32 -0500

    Update general_cmd.sh

--- cmd/general_cmd.sh
+++ cmd/general_cmd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Run a cmd using service account credentials.
+# Run a cmd using service account credentials as application credentials.
 
 set -x
 set -e
@@ -8,20 +8,17 @@ set -e
 PROJECT=${1:?Please provide the GCP project id}
 KEYNAME=${2:?Please provide the service account keyname}
 BASEDIR=${3:?Please provide the path to the command to run}
-CMD=${4:?Please provide the binary name to run}
+shift 3
 
 local keyfile=$( mktemp )
 set +x; echo "${!KEYNAME}" > ${keyfile}
 export GOOGLE_APPLICATION_CREDENTIALS=${keyfile}
 
+set -x
 
-# Make build artifacts available to docker build.
 pushd "${BASEDIR}"
-
-  # Automatically promote the new version to "serving".
-  # For all options see:
-  # https://cloud.google.com/sdk/gcloud/reference/app/deploy
-  GCLOUD_PROJECT="${PROJECT}" "${CMD}"
+  # Run command given on the rest of the command line.
+  GCLOUD_PROJECT="${PROJECT}" $@
 popd
 
 exit 0
