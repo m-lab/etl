43cc543776c29edd89280a58127555f7979360f1
Gregory Russell <gfr@google.com>
2020-03-03 15:22:32 -0500

    Allow concurrent encoding (by second client) while writing

--- storage/rowwriter.go
+++ storage/rowwriter.go
@@ -1,12 +1,11 @@
 package storage
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 
 	"github.com/googleapis/google-cloud-go-testing/storage/stiface"
-
-	"github.com/tidwall/pretty"
 )
 
 // ObjectWriter creates a writer to a named object.
@@ -21,12 +20,17 @@ func ObjectWriter(ctx context.Context, client stiface.Client, bucket string, pat
 	return w
 }
 
+// RowWriter implements row.Sink to a GCS file backend.
 type RowWriter struct {
-	w        stiface.Writer
+	w stiface.Writer
+	// These act as tokens to serialize access to the writer.
+	// This allows concurrent encoding and writing, while ensuring
+	// that single client access is correctly ordered.
 	encoding chan struct{} // Token required for metric updates.
 	writing  chan struct{} // Token required for metric updates.
 }
 
+// NewRowWriter creates a RowWriter.
 func NewRowWriter(ctx context.Context, client stiface.Client, bucket string, path string) (*RowWriter, error) {
 	w := ObjectWriter(ctx, client, bucket, path)
 	encoding := make(chan struct{}, 1)
@@ -39,42 +43,52 @@ func NewRowWriter(ctx context.Context, client stiface.Client, bucket string, pat
 
 // Acquire the encoding token.
 // TODO can we allow two encoders, and still sequence the writing?
-func (rw *RowWriter) encode() {
+func (rw *RowWriter) encodeToken() {
 	<-rw.encoding
 }
 
-// Swap the encoding token for the writing token.
-// MUST already hold the writing token.
-func (rw *RowWriter) write() {
+// Swap the encoding token for the write token.
+// MUST already hold the write token.
+func (rw *RowWriter) writeToken() {
 	<-rw.writing
 	rw.encoding <- struct{}{}
 }
 
-func (rw *RowWriter) done() {
+func (rw *RowWriter) release() {
 	rw.writing <- struct{}{} // return the token.
 }
 
 // Commit commits rows, in order, to the GCS object.
 func (rw *RowWriter) Commit(rows []interface{}, label string) error {
-	rw.encode()
-	// Do the encoding
-	var j []byte
+	rw.encodeToken()
+	// First, do the encoding.  Other calls to Commit will block here
+	// until encoding is done.
+	// NOTE: This can cause a fairly hefty memory footprint for
+	// large numbers of large rows.
+	buf := bytes.NewBuffer(nil)
+
 	for i := range rows {
-		var err error
-		j, err = json.Marshal(rows[i])
+		j, err := json.Marshal(rows[i])
 		if err != nil {
 			return err
 		}
-		pretty.UglyInPlace(j)
-		j := append(j, '\n')
-		rw.w.Write(j) // This is buffered (by 4MB chunks).  Are the writes to GCS synchronous?
+		json.Compact(buf, j)
+		buf.WriteByte('\n')
+	}
+	// Exchange the encoding token for the writing token.  Another caller
+	// will be allowed to start encoding.
+	rw.writeToken()
+	_, err := buf.WriteTo(rw.w) // This is buffered (by 4MB chunks).  Are the writes to GCS synchronous?
+	if err != nil {
+		return err
 	}
 
-	rw.write()
-	rw.done()
+	// Release the write token.  Another caller will be allowed to start writing.
+	rw.release()
 	return nil
 }
 
+// Close synchronizes on the tokens, and closes the backing file.
 func (rw *RowWriter) Close() error {
 	// Take BOTH tokens, to ensure no other goroutines are still running.
 	<-rw.encoding
