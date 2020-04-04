b2d998f56c8bbea72565d14d74acef2533ab8293
Gregory Russell <gfr@google.com>
2020-04-04 17:50:33 -0400

    clean up tar file

--- testfiles/sync.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# NOTE: This only syncs the ndt prefix.  All other prefixes are unaffected.
-
-set -x
-set -e
-tar xf ndt.tar
-gsutil -m rsync -R -P -d ndt gs://archive-mlab-testing/ndt/
