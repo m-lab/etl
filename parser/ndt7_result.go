1291b51824d457bb0c1c5235fec077f854591fe2
Stephen Soltesz <stephen.soltesz@gmail.com>
2020-04-10 12:56:14 -0400

    Fix docstring

--- parser/ndt7_result.go
+++ parser/ndt7_result.go
@@ -56,7 +56,7 @@ func (dp *NDT7ResultParser) TaskError() error {
 
 // IsParsable returns the canonical test type and whether to parse data.
 func (dp *NDT7ResultParser) IsParsable(testName string, data []byte) (string, bool) {
-	// Files look like: "<UUID>.json"
+	// Files look like:
 	// ndt7-{upload,download}-YYYYMMDDTHHMMSS.066461502Z.<UUID>.json.gz
 	if strings.HasPrefix(testName, "ndt7") && (strings.HasSuffix(testName, "json.gz") || strings.HasSuffix(testName, "json")) {
 		return "ndt7_result", true
