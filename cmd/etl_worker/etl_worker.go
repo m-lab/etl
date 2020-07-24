f0e5592cd166e32fb4ef4650163b5fc7f87c6788
Gregory Russell <gfr10598@users.noreply.github.com>
2020-07-23 22:32:56 -0400

    Revert "refactor main, unit test, more status info (#911)" (#917)
    This reverts commit 705f01ce8937f42b6be855a90a06a5c6c99fe854.

--- cmd/etl_worker/etl_worker.go
+++ cmd/etl_worker/etl_worker.go
@@ -15,10 +15,8 @@ import (
 	"time"
 
 	gcs "cloud.google.com/go/storage"
-	"golang.org/x/sync/errgroup"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
-	"github.com/m-lab/go/flagx"
-	"github.com/m-lab/go/httpx"
 	"github.com/m-lab/go/prometheusx"
 	"github.com/m-lab/go/rtx"
 
@@ -43,19 +41,6 @@ func init() {
 	// Always prepend the filename and line number.
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
-
-var (
-	maxActiveTasks = flag.Int64("max_active", 1, "Maximum number of active tasks")
-	gardenerHost   = flag.String("gardener_host", "", "Gardener host for jobs")
-
-	servicePort     = flag.String("service_port", ":8080", "The main (private) service port")
-	shutdownTimeout = flag.Duration("shutdown_timeout", 1*time.Minute, "Graceful shutdown time allowance")
-
-	mainCtx, mainCancel = context.WithCancel(context.Background())
-
-	// In active polling mode, this holds the GardenerAPI.
-	gardenerAPI *active.GardenerAPI
-)
 
 // Task Queue can always submit to an admin restricted URL.
 //   login: admin
@@ -84,10 +69,6 @@ func Status(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Release: %s   Commit: unknown\n", os.Getenv("RELEASE_TAG"))
 	}
 
-	if gardenerAPI != nil {
-		gardenerAPI.Status(w)
-	}
-	fmt.Fprintf(w, "Writing output to BigQuery\n")
 	fmt.Fprintf(w, "<p>Workers: %d / %d</p>\n", atomic.LoadInt32(&inFlight), maxInFlight)
 	env := os.Environ()
 	for i := range env {
@@ -274,12 +255,9 @@ func toRunnable(obj *gcs.ObjectAttrs) active.Runnable {
 	if err != nil {
 		return nil // TODO add an error?
 	}
-
-	sink := bq.NewSinkFactory()
-
 	taskFactory := worker.StandardTaskFactory{
 		Annotator: factory.DefaultAnnotatorFactory(),
-		Sink:      sink,
+		Sink:      bq.NewSinkFactory(),
 		Source:    storage.GCSSourceFactory(c),
 	}
 	return &runnable{&taskFactory, *obj}
@@ -293,74 +271,40 @@ func mustGardenerAPI(ctx context.Context, jobServer string) *active.GardenerAPI 
 	return active.NewGardenerAPI(*base, active.MustStorageClient(ctx))
 }
 
-// Used for testing.
-var mainServerAddr = make(chan string, 1)
-
-// startServers does not return until context is cancelled.
-func startServers(ctx context.Context, mux http.Handler) *errgroup.Group {
-	// Expose prometheus and pprof metrics on a separate port.
-	promServer := prometheusx.MustServeMetrics()
-	defer promServer.Close() // Only relevant if ListenAndServeAsync fails below.
-
-	// Start up the main job and update server.
-	server := &http.Server{
-		Addr:    *servicePort, // This used to be :8080
-		Handler: mux,
-	}
-
-	rtx.Must(httpx.ListenAndServeAsync(server), "Could not start main server")
-	// This publishes the service port for use in unit tests.
-	mainServerAddr <- server.Addr
-
-	select {
-	case <-ctx.Done():
-		// This currently only executes when the context is cancelled
-		// by unit tests.  It does not yet execute in production.
-		log.Println("Shutting down servers")
-		ctx, cancel := context.WithTimeout(context.Background(), *shutdownTimeout)
-		defer cancel()
-		start := time.Now()
-		eg := errgroup.Group{}
-		eg.Go(func() error {
-			return server.Shutdown(ctx)
-		})
-		eg.Go(func() error {
-			return promServer.Shutdown(ctx)
-		})
-		eg.Wait()
-		log.Println("Shutdown took", time.Since(start))
-		return &eg
-	}
-}
+var mainCtx, mainCancel = context.WithCancel(context.Background())
 
 func main() {
 	defer mainCancel()
-
 	flag.Parse()
-	rtx.Must(flagx.ArgsFromEnv(flag.CommandLine), "Could not get args from env")
+
+	// Expose prometheus and pprof metrics on a separate port.
+	prometheusx.MustStartPrometheus(":9090")
+
+	http.HandleFunc("/", Status)
+	http.HandleFunc("/status", Status)
+	http.HandleFunc("/worker", metrics.DurationHandler("generic", handleRequest))
+	http.HandleFunc("/_ah/health", healthCheckHandler)
 
 	// Enable block profiling
 	runtime.SetBlockProfileRate(1000000) // One event per msec.
 
 	setMaxInFlight()
 
-	if len(*gardenerHost) > 0 {
-		log.Println("Using", *gardenerHost)
+	// We also setup another prometheus handler on a non-standard path. This
+	// path name will be accessible through the AppEngine service address,
+	// however it will be served by a random instance.
+	http.Handle("/random-metrics", promhttp.Handler())
+
+	gardener := os.Getenv("GARDENER_HOST")
+	if len(gardener) > 0 {
+		log.Println("Using", gardener)
+		maxWorkers := 120
 		minPollingInterval := 10 * time.Second
-		gardenerAPI = mustGardenerAPI(mainCtx, *gardenerHost)
+		gapi := mustGardenerAPI(mainCtx, gardener)
 		// Note that this does not currently track duration metric.
-		go gardenerAPI.Poll(mainCtx, toRunnable, (int)(*maxActiveTasks), minPollingInterval)
+		go gapi.Poll(mainCtx, toRunnable, maxWorkers, minPollingInterval)
 	} else {
-		log.Println("GARDENER_HOST not specified or empty.  Running in passive mode.")
+		log.Println("GARDENER_HOST not specified or empty")
 	}
-
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", Status)
-	mux.HandleFunc("/status", Status)
-	mux.HandleFunc("/worker", metrics.DurationHandler("generic", handleRequest))
-	mux.HandleFunc("/_ah/health", healthCheckHandler) // legacy
-	mux.HandleFunc("/alive", healthCheckHandler)
-	mux.HandleFunc("/ready", healthCheckHandler)
-
-	_ = startServers(mainCtx, mux)
+	rtx.Must(http.ListenAndServe(":8080", nil), "failed to listen")
 }
