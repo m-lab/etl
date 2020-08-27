f55da1f70e6e7b6d0a86b52f6df98ec2eb7b3a02
Nathan Kinkade <kinkade@measurementlab.net>
2020-08-27 13:58:53 -0600

    Changes int64 'timestamp's to time.Time type.

--- schema/switch_schema.go
+++ schema/switch_schema.go
@@ -16,9 +16,9 @@ import (
 // uint64, but BigQuery does not support the notion of unsigned integers, so we
 // use int64 here, which should be safe, too.
 type Sample struct {
-	Timestamp int64   `json:"timestamp" bigquery:"timestamp"`
-	Value     float64 `json:"value" bigquery:"value"`
-	Counter   int64   `json:"counter" bigquery:"counter"`
+	Timestamp time.Time `json:"timestamp" bigquery:"timestamp"`
+	Value     float64   `json:"value" bigquery:"value"`
+	Counter   int64     `json:"counter" bigquery:"counter"`
 }
 
 // SwitchStats represents a row of data taken from the raw DISCO export file.
@@ -27,7 +27,7 @@ type SwitchStats struct {
 	TestID        string    `json:"test_id" bigquery:"test_id"`
 	ParseTime     time.Time `json:"parse_time" bigquery:"parse_time"`
 	ParserVersion string    `json:"parser_version" bigquery:"parser_version"`
-	LogTime       int64     `json:"log_time" bigquery:"log_time"`
+	LogTime       time.Time `json:"log_time" bigquery:"log_time"`
 	Sample        []Sample  `json:"sample" bigquery:"sample"`
 	Metric        string    `json:"metric" bigquery:"metric"`
 	Hostname      string    `json:"hostname" bigquery:"hostname"`
