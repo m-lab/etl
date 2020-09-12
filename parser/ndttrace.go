a09ff7b4cafb5d1eca5bbbbc07279149ae6aa4a1
Gregory Russell <gfr@google.com>
2020-09-12 12:19:51 -0400

    more ndttrace functionality

--- parser/ndttrace.go
+++ parser/ndttrace.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"log"
 	"strings"
 	"time"
@@ -74,23 +75,37 @@ func (ap *NDTTraceToAnnotation) ParseAndInsert(meta map[string]bigquery.Value, t
 	synth := annotations{}
 
 	// Construct UUID from filename
-	synth.UUID = ""
-	synth.Client.
+	// prefix/20180101T00:00:38.714313000Z_68.170.88.57.c2s_ndttrace.gz
+	parts := etl.ParseTestName(testName)
+	if len(parts) == 5 {
+		// TODO Get the Client IP from the suffix
+		client := "foobar"
+		t, err := time.Parse("20060102T15:04:05", fmt.Sprintf("%sT%sZ", parts[2], parts[3]))
+		if err != nil {
 
-		// NOTE: annotations are joined with other tables using the UUID, so
-		// finegrain timestamp is not necessary.
-		//
-		// NOTE: Civil is not TZ adjusted. It takes the year, month, and date from
-		// the given timestamp, regardless of the timestamp's timezone. Since we
-		// run our systems in UTC, all timestamps will be relative to UTC and as
-		// will these dates.
-		row.Date = meta["date"].(civil.Date)
+		}
+		synth.Timestamp = t
+		synth.UUID = fmt.Sprintf("%s-%s-%s-%sT%sZ", meta["site"], meta["host"], client, parts[2], parts[3][0:8])
+
+		synth.clientIP = suffix // Fix this with a regex
+	}
+
+	// NOTE: annotations are joined with other tables using the UUID, so
+	// finegrain timestamp is not necessary.
+	//
+	// NOTE: Civil is not TZ adjusted. It takes the year, month, and date from
+	// the given timestamp, regardless of the timestamp's timezone. Since we
+	// run our systems in UTC, all timestamps will be relative to UTC and as
+	// will these dates.
+	synth.Date = meta["date"].(civil.Date)
+	synth.meta["site"] = meta["site"].(string)
+	synth.meta["host"] = meta["host"].(string)
 
 	// Estimate the row size based on the input JSON size.
 	metrics.RowSizeHistogram.WithLabelValues(ap.TableName()).Observe(float64(len(test)))
 
 	// Insert the row.
-	if err = ap.Base.Put(&row); err != nil {
+	if err := ap.Base.Put(&synth); err != nil {
 		return err
 	}
 
@@ -141,9 +156,12 @@ func (ap *NDTTraceToAnnotation) Failed() int {
 
 type annotations struct {
 	annotator.Annotations
+	// https://siteinfo.mlab-oti.measurementlab.net/v1/sites/hostnames.json
 	serverIP string // Used by Annotatable
 	clientIP string // Used by Annotatable
-	logTime  time.Time
+
+	meta map[string]string
+	Date civil.Date `json:"-"` // For when we add standard columns
 }
 
 func (row *annotations) GetClientIPs() []string {
@@ -151,23 +169,56 @@ func (row *annotations) GetClientIPs() []string {
 }
 
 func (row *annotations) GetServerIP() string {
+	// Use https://siteinfo.mlab-oti.measurementlab.net/v1/sites/hostnames.json
 	return row.serverIP
 }
 
 func (row *annotations) AnnotateClients(remote map[string]*api.Annotations) error {
-	row.Client = remote[row.GetClientIPs()[0]]
+	row.Client.Geo = &annotator.Geolocation{}
+	row.Client.Network = &annotator.Network{}
+	ann, ok := remote[row.clientIP]
+	if !ok {
+		row.Client.Geo.Missing = true
+		row.Client.Network.Missing = true
+		return nil
+	}
+	row.Client.Geo.AccuracyRadiusKm = ann.Geo.AccuracyRadiusKm
+	row.Client.Geo.AreaCode = ann.Geo.AreaCode
+	row.Client.Geo.City = ann.Geo.City
+	row.Client.Geo.ContinentCode = ann.Geo.ContinentCode
+	row.Client.Geo.CountryCode = ann.Geo.CountryCode
+	row.Client.Geo.CountryCode3 = ann.Geo.CountryCode3
+	row.Client.Geo.CountryName = ann.Geo.CountryName
+	row.Client.Geo.Latitude = ann.Geo.Latitude
+	row.Client.Geo.Longitude = ann.Geo.Longitude
+	row.Client.Geo.MetroCode = ann.Geo.MetroCode
+	row.Client.Geo.PostalCode = ann.Geo.PostalCode
+	row.Client.Geo.Region = ann.Geo.Region
+	// row.Client.Geo.Subdivision1ISOCode =
+	// row.Client.Geo.Subdivision1Name
+	// row.Client.Geo.Subdivision2ISOCode
+	// row.Client.Geo.Subdivision2Name
 	return nil
 }
 
 func (row *annotations) AnnotateServer(local *api.GeoData) error {
-	row.Server = local
+	// Can we ignore the service info and look up by site/machine?
+	row.Server.Machine = row.meta["host"]
+	row.Server.Site = row.meta["site"]
+	//  https://siteinfo.mlab-oti.measurementlab.net/v1/sites/annotations.json
+	row.Server.Geo = &annotator.Geolocation{}
+	// https://siteinfo.mlab-oti.measurementlab.net/v1/sites/hostnames.json
+	// https://siteinfo.mlab-oti.measurementlab.net/v1/sites/annotations.json
+	row.Server.Network = &annotator.Network{}
+	row.Server.Geo.Missing = true
+	row.Server.Network.Missing = true
 	return nil
 }
 
 func (row *annotations) GetLogTime() time.Time {
-	return row.logTime
+	return row.Timestamp
 }
 
-func assertTestRowAnnotatable(r *Row) {
-	func(annotatable) {}(r)
+func assertTestRowAnnotatable(r *annotations) {
+	func(row.Annotatable) {}(r)
 }
