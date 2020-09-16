85641c60858d01be236dce23628f29b8a720a4fa
Gregory Russell <gfr10598@users.noreply.github.com>
2020-09-16 13:05:06 -0400

    Add NDT5ResultRowStandardColumns (#956)
    * Add NDT5ResultRowV1
    
    * add descriptions and unit test
    
    * Add unit test enumeration of fields without descriptions
    
    * add test comments
    
    * V1 -> StandardColumns, missing descriptions
    
    * add update command for NDT5StandardColumns
    
    * remove ndt.ndt5 update for now

--- schema/ndt5_result_test.go
+++ schema/ndt5_result_test.go
@@ -1,6 +1,7 @@
 package schema
 
 import (
+	"strings"
 	"testing"
 
 	"cloud.google.com/go/bigquery"
@@ -32,5 +33,48 @@ func TestNDT5Result_Schema(t *testing.T) {
 	})
 	if count != 3 {
 		t.Errorf("NDT5Result.Schema() missing expected fields; got %d, want 3", count)
+	}
+}
+
+func TestNDT5ResultV1_Schema(t *testing.T) {
+	row := &NDT5ResultRowStandardColumns{}
+	got, err := row.Schema()
+	if err != nil {
+		t.Errorf("NDT5ResultV1.Schema() error = %v, expected nil", err)
+		return
+	}
+	count := 0
+	fields := 0
+	descriptions := 0
+	// The complete schema is large, so verify that field descriptions
+	// are present for select fields by walking the schema and looking for them.
+	bqx.WalkSchema(got, func(prefix []string, field *bigquery.FieldSchema) error {
+		fields++
+		if field.Description != "" {
+			descriptions++
+		} else if field.Type == bigquery.RecordFieldType {
+			t.Logf("No description for %s", strings.Join(prefix, "."))
+		} else {
+			t.Logf("No description for %s.%s", strings.Join(prefix, "."), field.Name)
+		}
+		// a and parser are top level records
+		// GitShortCommit is a field in raw
+		for _, name := range []string{"id", "a", "parser", "GitShortCommit"} {
+			if field.Name == name {
+				if field.Description == "" {
+					t.Errorf("NDT5ResultV1.Schema() missing field.Description for %q", field.Name)
+				} else {
+					count++
+				}
+			}
+		}
+		return nil
+	})
+	if count != 4 {
+		t.Errorf("NDT5ResultV1.Schema() missing expected fields; got %d, want 4", count)
+	}
+	if descriptions != fields {
+		// Log if there are missing descriptions
+		t.Log(descriptions, "!=", fields)
 	}
 }
