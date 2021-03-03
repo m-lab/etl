c29bb092d6bc2cf88d5e5c81a61b1bf5a3271f87
Saied Kazemi <saied@google.com>
2021-03-03 13:46:58 -0800

    Get rid of the extra double-quotes in JSON string values (#970) (#971)
    Tested the changes locally in the parser and schema directories:
    $ cd parser && go test .
    ok      github.com/m-lab/etl/parser     0.705s
    $ cd schema && go test .
    ok  	github.com/m-lab/etl/schema	0.049s

--- schema/pt_schema.go
+++ schema/pt_schema.go
@@ -13,10 +13,10 @@ import (
 )
 
 type HopIP struct {
-	IP          string `json:"ip,string"`
-	City        string `json:"city,string"`
-	CountryCode string `json:"country_code,string"`
-	Hostname    string `json:"hostname,string"`
+	IP          string `json:"ip"`
+	City        string `json:"city"`
+	CountryCode string `json:"country_code"`
+	Hostname    string `json:"hostname"`
 	ASN         uint32 `json:"asn,uint32"`
 }
 
@@ -26,7 +26,7 @@ type HopProbe struct {
 }
 
 type HopLink struct {
-	HopDstIP string     `json:"hop_dst_ip,string"`
+	HopDstIP string     `json:"hop_dst_ip"`
 	TTL      int64      `json:"ttl,int64"`
 	Probes   []HopProbe `json:"probes"`
 }
@@ -38,18 +38,18 @@ type ScamperHop struct {
 }
 
 type PTTest struct {
-	UUID           string       `json:"uuid,string" bigquery:"uuid"`
+	UUID           string       `json:"uuid" bigquery:"uuid"`
 	TestTime       time.Time    `json:"testtime"`
 	Parseinfo      ParseInfoV0  `json:"parseinfo"`
 	StartTime      int64        `json:"start_time,int64" bigquery:"start_time"`
 	StopTime       int64        `json:"stop_time,int64" bigquery:"stop_time"`
-	ScamperVersion string       `json:"scamper_version,string" bigquery:"scamper_version"`
+	ScamperVersion string       `json:"scamper_version" bigquery:"scamper_version"`
 	Source         ServerInfo   `json:"source"`
 	Destination    ClientInfo   `json:"destination"`
 	ProbeSize      int64        `json:"probe_size,int64"`
 	ProbeC         int64        `json:"probec,int64"`
 	Hop            []ScamperHop `json:"hop"`
-	ExpVersion     string       `json:"exp_version,string" bigquery:"exp_version"`
+	ExpVersion     string       `json:"exp_version" bigquery:"exp_version"`
 	CachedResult   bool         `json:"cached_result,bool" bigquery:"cached_result"`
 }
 
