e8b39150fea84630789f15b36d2bae5158f27c3e
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-03-17 11:30:21 -0400

    Make testing credentials a warning

--- integration-testing.sh
+++ integration-testing.sh
@@ -3,26 +3,15 @@
 # Exit on error.
 set -e
 
-# Install test credentials for authentication:
-# * gcloud commands will use the activated service account.
-# * Go libraries will use the GOOGLE_APPLICATION_CREDENTIALS.
 if [[ -z "$SERVICE_ACCOUNT_mlab_testing" ]] ; then
-  echo "ERROR: testing service account is unavailable."
-  exit 1
+  echo "WARNING: testing service account is unavailable."
+  echo "WARNING: not running integration tests."
+  exit 0
 fi
 
+# Go libraries use the GOOGLE_APPLICATION_CREDENTIALS.
 echo "$SERVICE_ACCOUNT_mlab_testing" > $PWD/creds.json
-# Make credentials available for Go libraries.
 export GOOGLE_APPLICATION_CREDENTIALS=$PWD/creds.json
-# TODO: update travis script to make this optional.
-if [[ ! -f ${HOME}/google-cloud-sdk/path.bash.inc ]] ; then
-  mkdir -p ${HOME}/google-cloud-sdk/ || :
-  touch ${HOME}/google-cloud-sdk/path.bash.inc || :
-fi
-# Make credentials available for gcloud commands.
-#travis/activate_service_account.sh SERVICE_ACCOUNT_mlab_testing
 
-# NOTE: do this after setting the service account.
-#gcloud config set project mlab-testing
-
+# Run integration tests.
 go test -v -tags=integration -coverprofile=_integration.cov ./...
