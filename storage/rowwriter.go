de95fb52b1497df3c5a6d160d3491ed0efb75562
Gregory Russell <gfr@google.com>
2021-04-14 09:19:41 -0400

    retry for up to 1 minute - constant backoff

--- storage/rowwriter.go
+++ storage/rowwriter.go
@@ -111,7 +111,9 @@ func (rw *RowWriter) Commit(rows []interface{}, label string) (int, error) {
 	defer rw.releaseWritingToken()
 
 	n, err := buf.WriteTo(rw.w) // This is buffered (by 4MB chunks).  Are the writes to GCS synchronous?
-	if err != nil {
+retry:
+	// Retry for up to 1 minute
+	for i := 0; i < 30 && err != nil; i++ {
 		switch typedErr := err.(type) {
 		case *googleapi.Error:
 			// Retry the write...
@@ -121,9 +123,11 @@ func (rw *RowWriter) Commit(rows []interface{}, label string) (int, error) {
 			n2, err = buf.WriteTo(rw.w)
 			if err == nil {
 				log.Println("Retry succeeded", n, n2)
+				break retry
 			}
 			n += n2
 		default:
+			break retry
 		}
 	}
 	if err != nil {
