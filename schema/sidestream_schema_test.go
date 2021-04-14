bb0828970b9d6964428c19157ea4ac6347ce2229
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-04-14 16:50:14 -0400

    Check schema length

--- schema/sidestream_schema_test.go
+++ schema/sidestream_schema_test.go
@@ -8,6 +8,9 @@ func TestSS_Schema(t *testing.T) {
 	t.Run("schema-generate", func(t *testing.T) {
 		ss := &SS{}
 		sch, _ := ss.Schema()
+		if len(sch) < 3 {
+			t.Fatalf("SS.Schema() wrong length = got %d, want >= 3", len(sch))
+		}
 		if sch[2].Type != "TIMESTAMP" {
 			t.Errorf("SS.Schema() wrong log_time timestamp = got %q, want %q", sch[2].Type, "TIMESTAMP")
 		}
