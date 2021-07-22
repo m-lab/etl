04feeb6ec4b6d3958f9dd130ce288e684074589c
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-07-22 13:13:52 -0400

    Copy archive for integration tests

--- integration-testing.sh
+++ integration-testing.sh
@@ -13,5 +13,9 @@ fi
 echo "$SERVICE_ACCOUNT_mlab_testing" > $PWD/creds.json
 export GOOGLE_APPLICATION_CREDENTIALS=$PWD/creds.json
 
+# Prepare archives in mlab-testing project for some integration tests.
+gsutil cp testfiles/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz \
+    gs://archive-mlab-testing/ndt/ndt7/2021/06/17/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz
+
 # Run integration tests.
 go test -v -tags=integration -coverprofile=_integration.cov ./...
