a1438ba546b57873b4605eee5e6c075955053b5a
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-07-22 18:25:04 -0400

    Finalize script

--- integration-testing.sh
+++ integration-testing.sh
@@ -13,26 +13,16 @@ fi
 echo "$SERVICE_ACCOUNT_mlab_testing" > $PWD/creds.json
 export GOOGLE_APPLICATION_CREDENTIALS=$PWD/creds.json
 
-set -x
-
-gcloud auth list
-gcloud config configurations list
-ls -lR ~/.config/gcloud/
-
 # Prepare archives in mlab-testing project for some integration tests.
 source ./travis/gcloudlib.sh
 activate_service_account SERVICE_ACCOUNT_mlab_testing
-original=$(gcloud config get-value account)
 
-gcloud auth list
-gcloud config configurations list
-
+# Copy archive to GCS bucket.
 gsutil cp testfiles/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz \
     gs://archive-mlab-testing/ndt/ndt7/2021/06/17/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz
 
 # Run integration tests.
 go test -v -tags=integration -coverprofile=_integration.cov ./...
 
-
-gcloud auth revoke $(original)
-gcloud auth list
+# Revoke the service account credentials so to restore default credentials.
+gcloud auth revoke $(gcloud config get-value account)
