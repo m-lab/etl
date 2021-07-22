fc0b44b7fa4e50ae703420584158ce733b0a0983
Stephen Soltesz <stephen.soltesz@gmail.com>
2021-07-22 14:50:59 -0400

    Reset credentials

--- integration-testing.sh
+++ integration-testing.sh
@@ -13,11 +13,18 @@ fi
 echo "$SERVICE_ACCOUNT_mlab_testing" > $PWD/creds.json
 export GOOGLE_APPLICATION_CREDENTIALS=$PWD/creds.json
 
+gcloud config configurations list
+
 # Prepare archives in mlab-testing project for some integration tests.
 source ./travis/gcloudlib.sh
 activate_service_account SERVICE_ACCOUNT_mlab_testing
+
+gcloud config configurations list
+
 gsutil cp testfiles/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz \
     gs://archive-mlab-testing/ndt/ndt7/2021/06/17/20210617T003002.410133Z-ndt7-mlab1-foo01-ndt.tgz
 
 # Run integration tests.
 go test -v -tags=integration -coverprofile=_integration.cov ./...
+
+gcloud config configurations activate default
