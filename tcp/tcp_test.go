e61f8caf957d93cfe75c243ad8a61f6e8dc5ba7d
Gregory Russell <gfr@google.com>
2021-11-28 11:33:50 -0500

    check a few tcp stats

--- tcp/tcp_test.go
+++ tcp/tcp_test.go
@@ -1,12 +1,18 @@
 package tcp_test
 
 import (
+	"bytes"
+	"io/ioutil"
 	"log"
 	"math/rand"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcapgo"
 	"github.com/m-lab/etl/tcp"
+	"github.com/m-lab/etl/tcpip"
 )
 
 func init() {
@@ -87,5 +93,89 @@ func TestJitter(t *testing.T) {
 	}
 	if j.Jitter() > .005 || j.Jitter() < .001 {
 		t.Error(j.Jitter(), j.Delay(), j.ValCount)
+	}
+}
+
+func ProcessPackets(data []byte) (tcpip.Summary, error) {
+	summary := tcpip.Summary{
+		OptionCounts: make(map[layers.TCPOptionKind]int),
+		Errors:       make(map[int]error, 1),
+	}
+
+	pcap, err := pcapgo.NewReader(bytes.NewReader(data))
+	if err != nil {
+		log.Print(err)
+		return summary, err
+	}
+
+	summary.Details = make([]string, 0, 1000)
+
+	for data, ci, err := pcap.ReadPacketData(); err == nil; data, ci, err = pcap.ReadPacketData() {
+		// Pass ci by pointer, but Wrap will make a copy, since gopacket NoCopy doesn't preserve the values.
+		p, err := tcpip.Wrap(&ci, data)
+		if err != nil {
+			summary.Errors[summary.Packets] = err
+			continue
+		}
+		summary.Add(&p)
+	}
+	return summary, nil
+}
+
+// TestSummary exercises a lot of code.  It only checks
+// some basic stats, which are assumed to be correct, but not inspected.
+func TestSummary(t *testing.T) {
+	type test struct {
+		name                              string
+		fn                                string
+		packets                           int
+		leftRetransmits, rightRetransmits int64
+		truncated                         int64
+		exceeded                          int64
+		tsCount                           int64
+	}
+	tests := []test{
+		{name: "retransmits", fn: "testfiles/ndt-nnwk2_1611335823_00000000000C2DFE.pcap",
+			packets: 336, leftRetransmits: 11, rightRetransmits: 8, truncated: 0, exceeded: 0, tsCount: 510},
+		{name: "ipv6", fn: "testfiles/ndt-nnwk2_1611335823_00000000000C2DA8.pcap.gz",
+			packets: 15, leftRetransmits: 0, rightRetransmits: 0, truncated: 0, exceeded: 0, tsCount: 22},
+		{name: "protocolErrors2", fn: "testfiles/ndt-nnwk2_1611335823_00000000000C2DA9.pcap.gz",
+			packets: 5180, leftRetransmits: 0, rightRetransmits: 0, truncated: 0, exceeded: 2880, tsCount: 8542},
+		{name: "foobar", fn: "testfiles/ndt-xkrzj_1632230485_0000000000AE8EE2.pcap.gz",
+			packets: 49, leftRetransmits: 0, rightRetransmits: 0, truncated: 0, exceeded: 0, tsCount: 77},
+	}
+	for _, tt := range tests {
+		f, err := os.Open(tt.fn)
+		if err != nil {
+			t.Fatal(err)
+		}
+		data, err := ioutil.ReadAll(f)
+		if err != nil {
+			t.Fatal(err)
+		}
+		summary, err := ProcessPackets(data)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if summary.Packets != tt.packets {
+			t.Errorf("test:%s: Packets = %v, want %v", tt.name, summary.Packets, tt.packets)
+		}
+		// TODO - replace these with LeftStats and RightStats.
+		if summary.LeftState.Stats.RetransmitPackets != tt.leftRetransmits {
+			t.Errorf("test:%s: Left.Retransmits = %v, want %v", tt.name, summary.LeftState.Stats.RetransmitPackets, tt.leftRetransmits)
+		}
+		if summary.RightState.Stats.RetransmitPackets != tt.rightRetransmits {
+			t.Errorf("test:%s: Right.Retransmits = %v, want %v", tt.name, summary.RightState.Stats.RetransmitPackets, tt.rightRetransmits)
+		}
+		// if summary.TruncatedPackets != tt.truncated {
+		// 	t.Errorf("test:%s: TruncatedPackets = %v, want %v", tt.name, summary.TruncatedPackets, tt.truncated)
+		// }
+		if summary.RightState.Stats.SendNextExceededLimit != tt.exceeded {
+			t.Errorf("test:%s: SendNextExceededLimit = %v, want %v", tt.name, summary.RightState.Stats.SendNextExceededLimit, tt.exceeded)
+		}
+		if summary.LeftState.Stats.OptionCounts[layers.TCPOptionKindTimestamps] != tt.tsCount {
+			t.Errorf("test:%s: Timestamps = %v, want %v", tt.name,
+				summary.LeftState.Stats.OptionCounts[layers.TCPOptionKindTimestamps], tt.tsCount)
+		}
 	}
 }
