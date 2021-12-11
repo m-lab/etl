404762360eae2da331df79f284e8d614afb572b0
Gregory Russell <gfr@google.com>
2021-12-11 12:10:06 -0500

    add new records in pcap schema

--- schema/pcap.go
+++ schema/pcap.go
@@ -1,17 +1,47 @@
 package schema
 
 import (
+	"time"
+
 	"cloud.google.com/go/bigquery"
 	"cloud.google.com/go/civil"
 	"github.com/m-lab/etl/row"
 	"github.com/m-lab/go/cloud/bqx"
 )
 
+// PCAPSummary is used for the 'a' field, and contains summary data
+// describing high level characteristics of the connection.
+type PCAPSummary struct {
+	StartTime       time.Time
+	EndTime         time.Time
+	PacketsSent     int // requires IP header parsing
+	PacketsReceived int // requires IP header parsing
+}
+
+// PCAPRaw is used for the raw field, and contains basic information
+// derived trivially from the PCAP data.
+type PCAPRaw struct {
+	ClientIP   string
+	ClientPort int
+	ServerIP   string
+	ServerPort int
+}
+
+// PCAPExperimental is used for the exp field, and contains proposed
+// summary data that is not yet solid enough to put into the 'a' field.
+type PCAPExperimental struct {
+	// MinRTT float64
+}
+
 // PCAPRow describes a single BQ row of pcap (packet capture) data.
 type PCAPRow struct {
-	ID     string     `bigquery:"id"`
-	Parser ParseInfo  `bigquery:"parser"`
-	Date   civil.Date `bigquery:"date"`
+	ID     string     `bigquery:"id" json:"id"`
+	Parser ParseInfo  `bigquery:"parser" json:"parser"`
+	Date   civil.Date `bigquery:"date" json:"date"`
+
+	A   PCAPSummary      `bigquery:"a" json:"a"`
+	Raw PCAPRaw          `bigquery:"raw" json:"raw"`
+	Exp PCAPExperimental `bigquery:"exp" json:"exp"`
 
 	// NOT part of struct schema. Included only to provide a fake annotator interface.
 	row.NullAnnotator `bigquery:"-"`
