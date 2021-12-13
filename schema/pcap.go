daa139317a349f9586441aeb753921c5be1bb764
Gregory Russell <gfr@google.com>
2021-12-13 10:49:01 -0500

    remove unused pcap records

--- schema/pcap.go
+++ schema/pcap.go
@@ -18,32 +18,13 @@ type PCAPSummary struct {
 	PacketsReceived int // requires IP header parsing
 }
 
-// PCAPRaw is used for the raw field, and contains basic information
-// derived trivially from the PCAP data.
-type PCAPRaw struct {
-	ClientIP   string
-	ClientPort int
-	ServerIP   string
-	ServerPort int
-}
-
-// PCAPExperimental is used for the exp field, and contains proposed
-// summary data that is not yet solid enough to put into the 'a' field.
-type PCAPExperimental struct {
-	// Can't have an empty record, but we will use this later.
-	// MinRTT float64
-}
-
 // PCAPRow describes a single BQ row of pcap (packet capture) data.
 type PCAPRow struct {
 	ID     string     `bigquery:"id" json:"id"`
 	Parser ParseInfo  `bigquery:"parser" json:"parser"`
 	Date   civil.Date `bigquery:"date" json:"date"`
 
-	// For now, these are omitempty, but in future they should be unconditional.
-	A   PCAPSummary `bigquery:"a" json:"a,omitempty"`
-	Raw PCAPRaw     `bigquery:"raw" json:"raw,omitempty"`
-	//	Exp PCAPExperimental `bigquery:"exp" json:"exp,omitempty"`
+	A   PCAPSummary `bigquery:"a" json:"a"`
 
 	// NOT part of struct schema. Included only to provide a fake annotator interface.
 	row.NullAnnotator `bigquery:"-"`
