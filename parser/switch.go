718a08ec0e64562b8b7ed42eae91b9c41b9e7537
Roberto D'Auria <roberto@measurementlab.net>
2022-01-03 17:30:44 +0100

    Initial implementation of the new switch datatype parser.

---
+++ parser/switch.go
@@ -0,0 +1,218 @@
+package parser
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"strings"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+	v2as "github.com/m-lab/annotation-service/api/v2"
+	"github.com/m-lab/etl/etl"
+	"github.com/m-lab/etl/metrics"
+	"github.com/m-lab/etl/row"
+	"github.com/m-lab/etl/schema"
+)
+
+//=====================================================================================
+//                       Switch Datatype Parser
+//=====================================================================================
+
+// SwitchParser handles parsing for the switch datatype.
+type SwitchParser struct {
+	*row.Base
+	table  string
+	suffix string
+}
+
+// NewSwitchParser returns a new parser for the switch archives.
+func NewSwitchParser(sink row.Sink, table, suffix string, ann v2as.Annotator) etl.Parser {
+	bufSize := etl.SW.BQBufferSize()
+	if ann == nil {
+		ann = v2as.GetAnnotator(etl.BatchAnnotatorURL)
+	}
+
+	return &SwitchParser{
+		Base:   row.NewBase(table, sink, bufSize, ann),
+		table:  table,
+		suffix: suffix,
+	}
+}
+
+// IsParsable returns the canonical test type and whether to parse data.
+func (p *SwitchParser) IsParsable(testName string, data []byte) (string, bool) {
+	// Files look like: "<date>-to-<date>-switch.json.gz"
+	// Notice the "-" before switch.
+	// Look for JSON and JSONL files.
+	if strings.HasSuffix(testName, "switch.json") ||
+		strings.HasSuffix(testName, "switch.jsonl") ||
+		strings.HasSuffix(testName, "switch.json.gz") ||
+		strings.HasSuffix(testName, "switch.jsonl.gz") {
+		return "switch", true
+	}
+	return "", false
+}
+
+// ParseAndInsert decodes the switch data and inserts it into BQ.
+func (p *SwitchParser) ParseAndInsert(fileMetadata map[string]bigquery.Value, testName string, rawContent []byte) error {
+	metrics.WorkerState.WithLabelValues(p.TableName(), string(etl.SW)).Inc()
+	defer metrics.WorkerState.WithLabelValues(p.TableName(), string(etl.SW)).Dec()
+
+	reader := bytes.NewReader(rawContent)
+	dec := json.NewDecoder(reader)
+	rowCount := 0
+
+	// Each file contains multiple samples referring to the same hostname, but
+	// different timestamps. This map groups samples in rows by timestamp.
+	timestampToRow := make(map[int64]*schema.SwitchRow)
+
+	for dec.More() {
+		// Unmarshal the raw JSON into DISCOv2's Model.
+		// This can also hold DISCOv1 data. (XXX: is it true?)
+		tmp := &schema.SwitchStats{}
+		err := dec.Decode(tmp)
+		if err != nil {
+			metrics.TestCount.WithLabelValues(
+				p.TableName(), string(etl.SW), "Decode").Inc()
+			// TODO(dev) Should accumulate errors, instead of aborting?
+			return err
+		}
+
+		// For collectd in the "utilization" experiment, by design, the raw data
+		// time range starts and ends on the hour. This means that the raw
+		// dataset inclues 361 time bins (360 + 1 extra). Originally, this was
+		// so the last sample of the current time range would overlap with the
+		// first sample of the next time range. However, this parser does not
+		// use the extra sample, so we unconditionally ignore it here. However,
+		// this is not the case for DISCOv2, so we use the whole sample from
+		// DISCOv2. DISCOv2 can be differentiated from collectd by the "jsonl"
+		// suffix.
+		if len(tmp.Sample) > 0 {
+			if !strings.HasSuffix(testName, "switch.jsonl") &&
+				!strings.HasSuffix(testName, "switch.jsonl.gz") {
+				tmp.Sample = tmp.Sample[:len(tmp.Sample)-1]
+				// DISCOv1's Timestamp field in each sample represents the
+				// *beginning* of a 10s sample window, while v2's Timestamp
+				// represents the time at which the sample was taken, which is
+				// representative of the previous 10s. Since v2's behavior is
+				// what we want, we add 10s to all v1 Timestamps so that the
+				// timestamps represent the same thing for v1 and v2.
+				for i, v := range tmp.Sample {
+					tmp.Sample[i].Timestamp = v.Timestamp + 10
+				}
+			}
+		}
+
+		// Iterate over the samples in the JSON.
+		for _, sample := range tmp.Sample {
+			// If a row for this timestamp does not exist already, create one.
+			var row *schema.SwitchRow
+			var ok bool
+			if row, ok = timestampToRow[sample.Timestamp]; !ok {
+				row = &schema.SwitchRow{
+					Parser: schema.ParseInfo{
+						Version:    Version(),
+						Time:       time.Now(),
+						ArchiveURL: fileMetadata["filename"].(string),
+						Filename:   testName,
+						GitCommit:  GitCommit(),
+					},
+					Raw: &schema.RawData{
+						Metrics: []*schema.SwitchStats{},
+					},
+				}
+				timestampToRow[sample.Timestamp] = row
+			}
+
+			// Set the SwitchRow's timestamp from the sample's timestamp.
+			row.Date = time.Unix(sample.Timestamp, 0)
+
+			// Create a Model containing only this sample and append it to
+			// the current SwitchRow's Raw.Metrics field.
+			model := &schema.SwitchStats{
+				Experiment: tmp.Experiment,
+				Hostname:   tmp.Hostname,
+				Metric:     tmp.Metric,
+				Sample:     []schema.Sample{sample},
+			}
+			row.Raw.Metrics = append(row.Raw.Metrics, model)
+		}
+
+	}
+
+	for _, row := range timestampToRow {
+		rowCount++
+
+		// Count the number of samples per record.
+		metrics.DeltaNumFieldsHistogram.WithLabelValues(
+			p.TableName()).Observe(float64(len(row.Raw.Metrics)))
+
+		// TODO: estimate row size.
+		// metrics.RowSizeHistogram.WithLabelValues(
+		// 	p.TableName()).Observe(float64(row.Size()))
+
+		// Insert the row.
+		err := p.Base.Put(row)
+		if err != nil {
+			return err
+		}
+		if err != nil {
+			switch t := err.(type) {
+			case bigquery.PutMultiError:
+				// TODO improve error handling??
+				metrics.TestCount.WithLabelValues(
+					p.TableName(), "disco", "insert-multi").Inc()
+				log.Printf("%v\n", t[0].Error())
+			default:
+				metrics.TestCount.WithLabelValues(
+					p.TableName(), "disco", "insert-other").Inc()
+			}
+			// TODO(dev) Should accumulate errors, instead of aborting?
+			return err
+		}
+		// Count successful inserts.
+		metrics.TestCount.WithLabelValues(p.TableName(), "disco", "ok").Inc()
+	}
+
+	// Measure the distribution of records per file.
+	metrics.EntryFieldCountHistogram.WithLabelValues(
+		p.TableName()).Observe(float64(rowCount))
+
+	return nil
+}
+
+// NB: These functions are also required to complete the etl.Parser interface
+// For SwitchParser, we just forward the calls to the Inserter.
+
+func (p *SwitchParser) Flush() error {
+	return p.Base.Flush()
+}
+
+func (p *SwitchParser) TableName() string {
+	return p.table
+}
+
+func (p *SwitchParser) FullTableName() string {
+	return p.table + p.suffix
+}
+
+// RowsInBuffer returns the count of rows currently in the buffer.
+func (p *SwitchParser) RowsInBuffer() int {
+	return p.GetStats().Pending
+}
+
+// Committed returns the count of rows successfully committed to BQ.
+func (p *SwitchParser) Committed() int {
+	return p.GetStats().Committed
+}
+
+// Accepted returns the count of all rows received through InsertRow(s).
+func (p *SwitchParser) Accepted() int {
+	return p.GetStats().Total()
+}
+
+// Failed returns the count of all rows that could not be committed.
+func (p *SwitchParser) Failed() int {
+	return p.GetStats().Failed
+}
--- schema/switch_schema.go
+++ schema/switch_schema.go
@@ -3,10 +3,96 @@ package schema
 import (
 	"cloud.google.com/go/bigquery"
 
+	"github.com/m-lab/etl/row"
 	"github.com/m-lab/go/cloud/bqx"
 
 	"time"
 )
+
+// SwitchRow represents a single row of Switch data, taken from the raw DISCO
+// exported file.
+type SwitchRow struct {
+	// Parser contains metadata about the parsing of this row.
+	Parser ParseInfo `bigquery:"parser"`
+	// Date is the mtime of the archive being parsed.
+	Date time.Time `bigquery:"date"`
+	// A is the SwitchSummary containing the parsed metrics.
+	A *SwitchSummary
+	// Raw is the raw data from the DISCO export file.
+	Raw *RawData `json:",omitempty" bigquery:"raw"`
+
+	// NOT part of struct schema. Included only to provide a fake annotator interface.
+	row.NullAnnotator `bigquery:"-"`
+}
+
+// SwitchSummary contains the parsed metrics, plus the machine/switch pair.
+type SwitchSummary struct {
+	// Machine is the hostname of the machine that collected the metrics.
+	Machine string
+
+	// Switch is the switch's hostname.
+	Switch string
+
+	// Timestamp is the collection timestamp.
+	Timestamp time.Time
+
+	// The following fields are parsed from the raw data.
+	SwitchOctetsUplinkRx  uint64
+	SwitchOctetsUplinkTx  uint64
+	SwitchOctetsLocalRx   uint64
+	SwitchOctetsLocalTx   uint64
+	SwitchUnicastUplinkRx uint64
+	SwitchUnicastUplinkTx uint64
+	SwitchUnicastLocalRx  uint64
+	SwitchUnicastLocalTx  uint64
+	SwitchErrorsUplinkRx  uint64
+	SwitchErrorsUplinkTx  uint64
+}
+
+// RawData wraps a slice of Sample objects.
+// Only the raw data directly used in this row is included. Since the JSONL
+// files contain multiple timestamps per file, we filter out the timestamps
+// that are not used in this row. This prevents data duplication in BQ.
+type RawData struct {
+	Metrics []*SwitchStats
+}
+
+// Estimate the size of the RawData BQ row in bytes.
+// Note: This assumes all the SwitchStats objects in the RawData have the
+// same number of samples. This is generally true within a single DISCO file.
+func (r *RawData) Size() int {
+	if len(r.Metrics) == 0 {
+		return 0
+	}
+	return (r.Metrics[0].Size() * len(r.Metrics))
+}
+
+// Schema returns the Bigquery schema for SwitchRow.
+func (row *SwitchRow) Schema() (bigquery.Schema, error) {
+	sch, err := bigquery.InferSchema(row)
+	if err != nil {
+		return bigquery.Schema{}, err
+	}
+
+	// The raw data from DISCO stores the timestamp of a sample as an integer (a
+	// UNIX timestamp), but BigQuery represent the value as type TIMESTAMP.
+	subs := map[string]bigquery.FieldSchema{
+		"timestamp": {
+			Name:        "timestamp",
+			Description: "",
+			Repeated:    false,
+			Required:    false,
+			Type:        "TIMESTAMP"},
+	}
+	c := bqx.Customize(sch, subs)
+
+	docs := FindSchemaDocsFor(row)
+	for _, doc := range docs {
+		bqx.UpdateSchemaDescription(c, doc)
+	}
+	rr := bqx.RemoveRequired(sch)
+	return rr, err
+}
 
 // Sample is an individual measurement taken by DISCO.
 // NOTE: the types of the fields in this struct differ from the types used
@@ -23,49 +109,14 @@ type Sample struct {
 
 // SwitchStats represents a row of data taken from the raw DISCO export file.
 type SwitchStats struct {
-	TaskFilename  string    `json:"task_filename" bigquery:"task_filename"`
-	TestID        string    `json:"test_id" bigquery:"test_id"`
-	ParseTime     time.Time `json:"parse_time" bigquery:"parse_time"`
-	ParserVersion string    `json:"parser_version" bigquery:"parser_version"`
-	LogTime       time.Time `json:"log_time" bigquery:"log_time"`
-	Sample        []Sample  `json:"sample" bigquery:"sample"`
-	Metric        string    `json:"metric" bigquery:"metric"`
-	Hostname      string    `json:"hostname" bigquery:"hostname"`
-	Experiment    string    `json:"experiment" bigquery:"experiment"`
+	Metric     string   `json:"metric" bigquery:"metric"`
+	Hostname   string   `json:"hostname" bigquery:"hostname"`
+	Experiment string   `json:"experiment" bigquery:"experiment"`
+	Sample     []Sample `json:"sample" bigquery:"sample"`
 }
 
 // Size estimates the number of bytes in the SwitchStats object.
 func (row *SwitchStats) Size() int {
-	return (len(row.TaskFilename) + len(row.TestID) + 8 +
-		12*len(row.Sample) + len(row.Metric) + len(row.Hostname) + len(row.Experiment))
-}
-
-// Schema returns the BigQuery schema for SwitchStats.
-func (row *SwitchStats) Schema() (bigquery.Schema, error) {
-	sch, err := bigquery.InferSchema(row)
-	if err != nil {
-		return bigquery.Schema{}, err
-	}
-
-	// The raw data from DISCO stores the timestamp of a sample as an integer (a
-	// UNIX timestamp), but BigQuery represent the value as type TIMESTAMP.
-	// TODO: DISCO should probably store the value as time.Time to avoid the
-	// need for this.
-	subs := map[string]bigquery.FieldSchema{
-		"timestamp": bigquery.FieldSchema{
-			Name:        "timestamp",
-			Description: "",
-			Repeated:    false,
-			Required:    false,
-			Type:        "TIMESTAMP"},
-	}
-	c := bqx.Customize(sch, subs)
-
-	docs := FindSchemaDocsFor(row)
-	for _, doc := range docs {
-		bqx.UpdateSchemaDescription(c, doc)
-	}
-	rr := bqx.RemoveRequired(c)
-
-	return rr, err
+	return (24*len(row.Sample) + len(row.Metric) + len(row.Hostname) +
+		len(row.Experiment))
 }
