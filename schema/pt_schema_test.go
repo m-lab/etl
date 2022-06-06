dd72fa7061238d77237fa159ac3c00b90b709ce8
Stephen Soltesz <stephen.soltesz@gmail.com>
2022-06-06 14:36:39 -0400

    Add TODO for pt schema

--- schema/pt_schema_test.go
+++ schema/pt_schema_test.go
@@ -1,1 +1,11 @@
 package schema_test
+
+/*
+
+The PTTest Schema is a v1 datatype, that does not support standard column convention.
+
+TODO:
+* migrate PTTest to v2 parser, using standard column conventions.
+* restore standard schema unit tests for v2 schema.
+
+*/
