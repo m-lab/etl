ce0e03653628d38c9b7617f60b7a7da6c4cb33ee
Stephen Soltesz <stephen.soltesz@gmail.com>
2023-06-01 15:58:04 -0400

    Remove obsolete type definition

--- schema/ndt5_result.go
+++ schema/ndt5_result.go
@@ -10,30 +10,6 @@ import (
 	"github.com/m-lab/ndt-server/data"
 )
 
-// NDT5ResultRow defines the BQ schema for the data.NDT5Result produced by the
-// ndt-server for NDT client measurements.
-// Deprecated - use V1 for Gardener 2.0
-type NDT5ResultRow struct {
-	ParseInfo *ParseInfoV0
-	TestID    string          `json:"test_id,string" bigquery:"test_id"`
-	LogTime   int64           `json:"log_time,int64" bigquery:"log_time"`
-	Result    data.NDT5Result `json:"result" bigquery:"result"`
-}
-
-// Schema returns the BigQuery schema for NDT5ResultRow.
-func (row *NDT5ResultRow) Schema() (bigquery.Schema, error) {
-	sch, err := bigquery.InferSchema(row)
-	if err != nil {
-		return bigquery.Schema{}, err
-	}
-	docs := FindSchemaDocsFor(row)
-	for _, doc := range docs {
-		bqx.UpdateSchemaDescription(sch, doc)
-	}
-	rr := bqx.RemoveRequired(sch)
-	return rr, err
-}
-
 // NDT5ResultRowV2 defines the BQ schema for the data.NDT5Result produced by the
 // ndt-server for NDT client measurements.
 type NDT5ResultRowV2 struct {
